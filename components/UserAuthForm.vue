1af6ac7d31349099b421de26f92dc57eb39612e8
Scott Kennedy <scottyzen@gmail.com>
2021-08-11 22:33:26 +0100

    Next update

---
+++ components/UserAuthForm.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="max-w-md mx-auto ">
+		<form class="grid gap-4 mb-4">
+			<input placeholder="Name" type="text" v-model="userInfo.name" />
+			<input placeholder="Email" type="email" v-model="userInfo.email" />
+			<input placeholder="Password" type="text" v-model="userInfo.password" />
+			<button @click="submitForm(userInfo)">{{ buttonText }}</button>
+		</form>
+		<button @click="googleSSO">Sign in with Google</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			userInfo: { name: '', email: '', password: '' }
+		}
+	},
+	methods: {
+		googleSSO() {
+			this.$auth.loginWith('google').then(() => {
+				console.log('you have been logged in.')
+			})
+		}
+	},
+	props: ['submitForm', 'buttonText']
+}
+</script>
+
+<style lang="postcss" scoped>
+input,
+button {
+	@apply p-4 rounded border w-full;
+}
+form button {
+	@apply p-4 bg-green-700 text-white rounded;
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="container">
+		<h1 class="py-12 text-3xl text-center">Login</h1>
+		<pre class="mb-12 text-xs">{{this.$auth.user}}</pre>
+		<UserAuthForm buttonText="Login" :submitForm="loginUser" />
+	</div>
+</template>
+
+<script>
+export default {
+	methods: {
+		loginUser(logininfo) {
+			alert('You pressed a button ', logininfo)
+		}
+	},
+	created() {
+		console.dir(this.$auth)
+	}
+}
+</script>
