51b15b765f50d0ca48a39aaf4fb9db3bc93ce917
Scott Kennedy <scottyzen@gmail.com>
2021-09-23 23:41:02 +0100

    Tweaked splat

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,8 +2,8 @@
   <footer>
     <div class="container py-8">
       <nav class="grid gap-1">
-        <a href="https://woonuxt.netlify.app/cart">Cart</a>
-        <a href="https://woonuxt.netlify.app/checkout">Checkout</a>
+        <a href="https://woonuxt.netlify.app/cart/">Cart</a>
+        <a href="https://woonuxt.netlify.app/checkout/">Checkout</a>
       </nav>
     </div>
   </footer>
