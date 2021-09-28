584696a1741c61215e21bb51861bea6b0b9459ef
Scott Kennedy <scottyzen@gmail.com>
2021-09-28 21:51:11 +0100

    Axios

--- components/Cart.vue
+++ components/Cart.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import { gql } from 'nuxt-graphql-request'
 import getCart from '~/gql/queries/getCart'
 const requestHeaders = {
@@ -28,20 +29,53 @@ export default {
 	},
 	methods: {
 		async getUser() {
-			const query = gql`
-				query {
+			// const query = gql`
+			// 	query {
+			// 		viewer {
+			// 			email
+			// 			firstName
+			// 		}
+			// 	}
+			// `
+
+			// axios.post(`https://testing.2cubedtest.com/grapghql`, { withCredentials: true }, query).then(
+			// 	(response) => {
+			// 		console.log(response)
+			// 	},
+			// 	(error) => {
+			// 		console.log(error)
+			// 	}
+			// )
+
+			// const data = await this.$graphql.default.rawRequest(query)
+			// const cart = await this.$graphql.default.request(getCart)
+			// this.cart = cart
+			// this.viewer = data.data.viewer
+			// console.log(data)
+
+			const data = {
+				query: `query {
 					viewer {
 						email
 						firstName
 					}
-				}
-			`
+				}`
+			}
 
-			const data = await this.$graphql.default.rawRequest(query)
-			const cart = await this.$graphql.default.request(getCart)
-			this.cart = cart
-			this.viewer = data.data.viewer
-			console.log(data)
+			const options = {
+				method: 'POST',
+				withCredentials: true,
+				headers: { 'content-type': 'application/json' },
+				data,
+				url: 'https://testing.2cubedtest.com/grapghql'
+			}
+
+			try {
+				const response = await axios(options)
+				console.log(response)
+			} catch (error) {
+				console.error(error)
+			}
 		}
 	}
 }
