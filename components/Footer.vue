a04649fde37a5c8260ac58738a9f1f0ba10b3b00
Scott Kennedy <scottyzen@gmail.com>
2021-09-28 23:21:01 +0100

    Footer links

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,9 +2,9 @@
 	<footer>
 		<div class="container py-8">
 			<nav class="grid gap-1">
-				<a href="https://woonuxt.com/my-account">SOO</a>
-				<a href="https://woonuxt.com/cart">Cart</a>
-				<a href="https://woonuxt.com/checkout">Checkout</a>
+				<a href="https://woonuxt.com/my-account/">My Account</a>
+				<a href="https://woonuxt.com/cart/">Cart</a>
+				<a href="https://woonuxt.com/checkout/">Checkout</a>
 			</nav>
 		</div>
 	</footer>
