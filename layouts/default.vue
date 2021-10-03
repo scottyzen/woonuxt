f4be9033c501ab0326ef070edeb2c4dcebbe27f9
Scott Kennedy <scottyzen@gmail.com>
2021-10-03 21:08:13 +0100

    New log out function

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,12 +37,15 @@ export default {
 		async logOut() {
 			console.log('Clear all cookies')
 			const deleteAllCookies = () => {
-				const cookies = document.cookie.split(';')
-				for (const cookie of cookies) {
-					const eqPos = cookie.indexOf('=')
-					const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+				var cookies = document.cookie.split(';')
+
+				for (var i = 0; i < cookies.length; i++) {
+					var cookie = cookies[i]
+					var eqPos = cookie.indexOf('=')
+					var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
 					document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT'
 				}
+				window.localStorage.clear()
 			}
 			deleteAllCookies()
 			this.$graphql.default.setHeaders({
