01be87541871cb4605041ef5deaced0cf20e911c
Scott Kennedy <scottyzen@gmail.com>
2021-10-10 21:36:20 +0100

    Better lighthouse

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,6 +1,6 @@
 <template>
 	<nuxt-link class="text-center" :to="`/product/${node.slug}`">
-		<nuxt-img class="w-full mb-2" :src="node.image.sourceUrl" :alt="node.name" :title="node.name" fit="cover" width="136" height="136" sizes="xs:136px sm:296px md:325px xl:400px" :loading="index == 0 ? 'eager': 'lazy'" quality="90" />
+		<nuxt-picture class="w-full mb-2" :src="node.image.sourceUrl" :alt="node.name" :title="node.name" fit="cover" width="136" height="136" sizes="xs:128px sm:296px md:325px xl:400px" :loading="index == 0 ? 'eager': 'lazy'" quality="90" format="webp" />
 		<div class="p-2">
 			<div class="text-sm">{{ node.name }}</div>
 			<strong>{{ node.price }}</strong>
@@ -25,3 +25,9 @@ export default {
 	}
 }
 </script>
+
+<style >
+picture.w-full img {
+	width: 100%;
+}
+</style>
