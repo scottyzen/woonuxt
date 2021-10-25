16e66d95272d7eb027d241df998d1bc434b297ce
Scott Kennedy <scottyzen@gmail.com>
2021-10-25 21:03:32 +0100

    Updated dependices

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,7 +4,7 @@
 			<nuxt-link to="/news">News</nuxt-link>
 			<nuxt-link to="/login">Sign In</nuxt-link>
 			<nuxt-link to="/logout">Log out</nuxt-link>
-			<a href="https://woonext.com/soo/">SSO</a>
+			<a href="https://woonuxt.com/soo/">SSO</a>
 		</div>
 		<a href="https://woonuxt.com" class="border-t text-xs text-center py-8 text-gray-700 block">WooNuxt Version 0.1.8.1</a>
 	</footer>
