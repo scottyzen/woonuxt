82b11443866b8ade0d4f769156044df882a8d222
Scott Kennedy <scottyzen@gmail.com>
2021-10-28 20:09:21 +0100

    Updaded dependices

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "theme-color", content: "#ffffff" },
       {
         hid: "description",
         name: "description",
