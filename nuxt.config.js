8688ee4ecccdfcf463a99430319070a2416b8d5b
Scott Kennedy <scottyzen@gmail.com>
2022-03-24 14:24:10 +0000

    Lighthouse test

--- nuxt.config.js
+++ nuxt.config.js
@@ -122,12 +122,6 @@ export default defineNuxtConfig({
   },
 
   build: {
-    aggressiveCodeRemoval: true,
-    extend(config, { isClient }) {
-      const isProd = process.env.NODE_ENV === 'production';
-      if (isProd && isClient) {
-        config.optimization.splitChunks.maxSize = 249856; // 244 Kib
-      }
-    },
+    aggressiveCodeRemoval: true
   }
 })
