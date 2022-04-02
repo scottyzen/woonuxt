6ddc704ad774ed5eb628276dc4dc52e0d9d955eb
Scott Kennedy <scottyzen@gmail.com>
2022-04-02 18:42:40 +0100

    Removed nuxt image

--- components/elements/Logo.vue
+++ components/elements/Logo.vue
@@ -1,7 +1,7 @@
 <template>
 	<NuxtLink to="/">
 		<h1 class="flex font-bold text-lg items-center">
-			<SCImg class="mr-2" width="32" height="32" src="logo.svg" alt="Logo" />WooNuxt
+			<img class="mr-2" width="32" height="32" src="logo.svg" alt="Logo" />WooNuxt
 		</h1>
 	</NuxtLink>
 </template>
