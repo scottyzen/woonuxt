2c9cc7712642366752fd83f0d194efd0d6be0683
Scott Kennedy <scottyzen@gmail.com>
2022-04-14 19:18:44 +0100

    Made category image larger

--- components/CategoryCard.vue
+++ components/CategoryCard.vue
@@ -1,23 +1,12 @@
 <template>
-    <NuxtLink
-        :to="`/product-category/${node.slug}`"
-        class="border border-white rounded-xl flex item relative justify-center overflow-hidden snap-mandatory snap-x"
-    >
-        <SCImg
-            v-if="node.image"
-            width="300"
-            height="300"
-            class="h-full object-cover w-full inset-0 absolute"
-            :src="node.image.sourceUrl"
-            :alt="node.name"
-            loading="lazy"
-        />
-        <div
-            class="bg-gradient-to-t from-black to-transparent h-1/2 opacity-50 inset-x-0 bottom-0 absolute"
-        ></div>
-        <span
-            class="font-semibold mt-auto text-sm text-white mb-2 z-10 relative capitalize md:text-base md:mb-4"
-        >{{ node.name }}</span>
+    <NuxtLink :to="`/product-category/${node.slug}`"
+        class="border border-white rounded-xl flex item relative justify-center overflow-hidden snap-mandatory snap-x">
+        <SCImg v-if="node.image" width="500" height="500" class="h-full object-cover w-full inset-0 absolute"
+            :src="node.image.sourceUrl" :alt="node.name" loading="lazy" />
+        <div class="bg-gradient-to-t from-black to-transparent h-1/2 opacity-50 inset-x-0 bottom-0 absolute"></div>
+        <span class="font-semibold mt-auto text-sm text-white mb-2 z-10 relative capitalize md:text-base md:mb-4">{{
+            node.name
+        }}</span>
     </NuxtLink>
 </template>
 
--- pages/categories.vue
+++ pages/categories.vue
@@ -1,14 +1,7 @@
 <template>
     <main class="container">
-        <div
-            class="my-4 grid gap-4 grid-cols-2 lg:gap-8 lg:grid-cols-3 xl:grid-cols-4"
-            :style="cssVars"
-        >
-            <CategoryCard
-                v-for="(category, i) in productCategories.nodes"
-                :key="i"
-                :node="category"
-            />
+        <div class="my-4 grid gap-4 grid-cols-2 lg:gap-8 lg:grid-cols-3 xl:grid-cols-4">
+            <CategoryCard v-for="(category, i) in productCategories.nodes" :key="i" :node="category" />
         </div>
     </main>
 </template>
