0e62b14f95fa1c5e27af5460a95b6e25f50bf168
Scott Kennedy <scottyzen@gmail.com>
2022-05-03 22:01:18 +0100

    Image tweaks

--- components/SCImg.vue
+++ components/SCImg.vue
@@ -1,5 +1,5 @@
 <template>
-    <img :src="sourceUrl" :width="width" :height="height" :srcset="srcset" />
+    <img :src="sourceUrl" :width="sWidth" :height="sHeight" :srcset="srcset" />
 </template>
 
 <script>
@@ -12,7 +12,7 @@ export default {
         quality: { type: String, default: '100' },
         format: { type: String, default: 'webp' },
         preload: { type: Boolean, default: false },
-        sm: { type: Number, default: 0 },
+        sm: { type: Number, default: null },
     },
     head() {
         return {
@@ -25,31 +25,29 @@ export default {
             return (process.env.NODE_ENV === 'production') ? `https://images.weserv.nl/?url=https://${this.$config.domain}/${this.src}` : this.src;
         },
         sourceUrl() {
-            if (this.sm) {
-                const ratio = this.width / this.height;
-                const smallWidth = this.sm;
-                const smallHeight = smallWidth / ratio;
-                return `${this.base}?w=${smallWidth}&h=${smallHeight}&output=${this.format}&q=${this.quality}&dpr=2`;
-            } else {
-                return `${this.base}?w=${this.width}&h=${this.height}&output=${this.format}&q=${this.quality}&dpr=2`
-            }
+            return `${this.base}?w=${this.sWidth}&h=${this.sHeight}&output=${this.format}&q=${this.quality}&dpr=2`
+        },
+        ratio() {
+            return this.width / this.height;
+        },
+        sHeight() {
+            return this.sm ? this.sm / this.ratio : this.height;
+        },
+        sWidth() {
+            return this.sm ? this.sm : this.width;
         },
         srcset() {
             const sizes = [320, 768, 1280];
 
-            const sizesBelowMaxWidth = sizes.filter(size => {
-                // if (size == 320 && this.sm) return true;
-                return size < parseInt(this.width)
-            });
+            const sizesBelowMaxWidth = sizes.filter(size => size < parseInt(this.width));
 
             const srsArray = sizesBelowMaxWidth.map(size => {
-                const ratio = this.width / this.height;
                 const width = size > parseInt(this.width) ? this.width : size;
-                const height = width / ratio;
+                const height = width / this.ratio;
 
                 // get small size ratio
                 const smallWidth = this.sm;
-                const smallHeight = smallWidth / ratio;
+                const smallHeight = smallWidth / this.ratio;
 
                 if (size <= 320 && this.sm) {
                     return `${this.base}?w=${smallWidth}&h=${smallHeight}&output=${this.format}&dpr=2 ${size}w`
