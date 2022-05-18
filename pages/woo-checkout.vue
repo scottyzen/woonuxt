838c1d940966d8df08d684b92a0b2c4c82961b81
Scott Kennedy <scottyzen@gmail.com>
2022-05-18 20:27:32 +0100

    WooCheckout test

---
+++ pages/woo-checkout.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="container">
+		<iframe src="https://woonuxt.com/woo-checkout/"></iframe>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
