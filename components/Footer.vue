eea7ce774df581303c6293e043c9203ba4c1a018
Scott Kennedy <scottyzen@gmail.com>
2022-05-18 20:09:39 +0100

    WooCheckout test

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,5 +2,6 @@
 	<footer class="bg-white border-t text-xs text-center py-8 text-gray-700 block">
 		<a href="https://woonuxt.com" class>WooNuxt v{{ $config.clientVersion }}</a> - by
 		<a href="https://scottyzen.com" target="_blank">Scottyzen</a>
+		<a href="https://woonuxt.com/woo-checkout" class="hidden">WooCheckout</a>
 	</footer>
 </template>
