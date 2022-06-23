59e6f900d185542390fecfde8a1a570406ad6866
Scott Kennedy <scottyzen@gmail.com>
2022-06-23 21:12:19 +0100

    Fix product image on safari

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -2,16 +2,16 @@
   <NuxtLink :to="{ name: 'product-slug', params: { slug: node.slug, page: page } }" class="relative">
     <SaleBadge :node="node" class="top-2 right-2 absolute" />
 
-    <NuxtImg
+    <NuxtPicture
       v-if="node.image"
       width="280"
       height="315"
-      class="rounded-xl object-top object-cover w-full product-image"
       :src="node.image.sourceUrl"
       :alt="node.image.altText || node.name"
       :title="node.image.title || node.name"
       :loading="index <= 1 || index == 5 ? 'eager' : 'lazy'"
       format="avif"
+      :imgAttrs="{ class: 'rounded-xl object-top object-cover w-full product-image' }"
       fit="outside"
     />
 
