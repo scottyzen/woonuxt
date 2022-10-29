9446c6a39d3cf780d841097ceca76ff51f56cd20
Zack Hatlen <zckhtln@icloud.com>
2022-10-29 11:57:47 +0300

    Update Breadcrumb.vue

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex text-sm leading-none text-gray-400 gap-1 items-center">
+  <div class="flex text-sm leading-none text-dark-200 gap-1 items-center">
     <!-- {{ format }} -->
     <span v-for="(link, i) in format" :key="link.name">
       <NuxtLink v-if="link.slug" :to="link.slug" class="hover:text-primary">{{ link.name }}</NuxtLink>
-      <span v-else class="text-gray-800">{{ link.name }}</span>
-      <span v-if="i + 1 < format.length">/</span>
+      <span v-else class=" text-dark-200 font-semibold">{{ link.name }}</span>
+      <span v-if="i + 1 < format.length" class="mx-2">/</span>
     </span>
   </div>
 </template>
