a0bd264f48148c19e724683041a167ef956bbd80
Zack Hatlen <zckhtln@icloud.com>
2022-10-29 17:12:11 +0300

    Update Breadcrumb.vue
    Co-Authored-By: Scott Kennedy <scottyzen@gmail.com>

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -3,7 +3,7 @@
     <!-- {{ format }} -->
     <span v-for="(link, i) in format" :key="link.name" class="contents">
       <NuxtLink v-if="link.slug" :to="link.slug" class="hover:text-primary hover:underline">{{ link.name }}</NuxtLink>
-      <span v-else class=" text-dark-200 font-semibold">{{ link.name }}</span>
+      <span v-else class=" text-dark-200 font-semibold truncate">{{ link.name }}</span>
       <span v-if="i + 1 < format.length" class="mx-1.75 w-1.25 h-1.5">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="63.925 63.925 224.1 384.1">
           <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" fill="#ae7ddd"/>
