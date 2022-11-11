401db759baffee3d81c2f9ba708779968f73754d
Scott Kennedy <scottyzen@gmail.com>
2022-11-11 21:35:47 +0000

    Add Fireworks to the Order summary page 🎆

--- pages/order-summary.vue
+++ pages/order-summary.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full p-8 text-gray-800 md:bg-white md:rounded-xl md:mx-auto md:shadow md:my-12 md:max-w-3xl md:p-16" v-if="order">
+  <div class="w-full p-8 text-gray-800 md:bg-white md:rounded-xl md:mx-auto md:shadow-lg md:mt-8 md:my-24 md:max-w-3xl md:p-16 z-50" v-if="order">
     <h1 v-if="$route.params.order" class="font-semibold text-xl mb-2">Order received</h1>
     <h1 v-else class="font-semibold text-xl mb-2">Order Summary</h1>
     <p v-if="$route.params.order">Thank you for your order. We will send you an email with the order details.</p>
@@ -29,11 +29,7 @@
 
     <div class="grid gap-2">
       <div v-for="item in order.lineItems.nodes" :key="item.product.databaseId" class="flex gap-8 items-center justify-between">
-        <NuxtImg
-          class="rounded-xl h-16 w-16"
-          v-if="item.product.node.image"
-          :src="item.variation ? item.variation.node.image.sourceUrl : item.product.node.image.sourceUrl"
-        />
+        <NuxtImg class="rounded-xl h-16 w-16" v-if="item.product.node.image" :src="item.variation ? item.variation.node.image.sourceUrl : item.product.node.image.sourceUrl" />
         <div class="flex-1 leading-tight">{{ item.variation ? item.variation.node.name : item.product.node.name }}</div>
         <div class="text-sm text-gray-600">Qty. {{ item.quantity }}</div>
         <span class="font-semibold text-sm">{{ formatPrice(item.total) }}</span>
@@ -68,7 +64,10 @@
 import GET_ORDER from "~/gql/queries/getOrder";
 export default {
   head() {
-    return { title: "Order received" };
+    return {
+      title: "Order Received",
+      script: [{ src: "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js" }],
+    };
   },
   data() {
     return {
@@ -79,6 +78,8 @@ export default {
     if (this.$route.query.id) {
       this.getOrder();
     }
+    // If the order is received, show confettis
+    this.showFireWorks();
   },
   methods: {
     async getOrder() {
@@ -101,6 +102,26 @@ export default {
         style: "currency",
         currency: "EUR",
       });
+    },
+    showFireWorks() {
+      var duration = 15 * 1000;
+      var animationEnd = Date.now() + duration;
+      var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+      function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+
+      var interval = setInterval(function () {
+        var timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) clearInterval(interval);
+
+        var particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti(Object.assign({}, defaults, { particleCount, zIndex: 49, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+        confetti(Object.assign({}, defaults, { particleCount, zIndex: 49, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+      }, 250);
     },
   },
 };
