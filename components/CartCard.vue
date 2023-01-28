71754f4479b0842322f1f3600e2208f7c64891d5
Scott Kennedy <scottyzen@gmail.com>
2023-01-28 13:48:17 +0000

    Fixed cart qantity bug

--- components/CartCard.vue
+++ components/CartCard.vue
@@ -1,11 +1,14 @@
 <template>
-  <li class="flex gap-4 items-center" v-if="item">
+  <li class="flex gap-4 items-center relative" v-if="item">
     <NuxtImg class="rounded-xl h-16 w-16" v-if="productType.image" :src="productType.image.sourceUrl" :alt="productType.image.altText || productType.name" :title="productType.image.altText || productType.name" />
     <div class="flex-1">
       <div class="leading-tight">{{ productType.name }}</div>
       <ProductPrice class="mt-1 text-xs" :salePrice="productType.salePrice" :regularPrice="productType.regularPrice" />
     </div>
     <QuantityButtons :quantity="quantity" @quantity-change="updateQuantity" />
+    <div class="bg-white flex bg-opacity-50 inset-0 absolute items-center justify-center" v-if="isLoading">
+      <LoadingIcon size="20" color="#8B5CF6" />
+    </div>
   </li>
 </template>
 
@@ -20,14 +23,19 @@ export default {
   props: ["item"],
   methods: {
     async updateQuantity(quantity) {
-      const quantityInt = quantity ? parseInt(quantity) : 0;
-      const { updateItemQuantities } = await this.$graphql.default.request(updateCartQuantity, { key: this.item.key, quantity: quantityInt });
-      this.$store.commit("updateCart", updateItemQuantities.cart);
+      try {
+        this.isLoading = true;
+        const { updateItemQuantities } = await this.$graphql.default.request(updateCartQuantity, { key: this.item.key, quantity: parseInt(quantity) });
+        this.$store.commit("updateCart", updateItemQuantities.cart);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.isLoading = false;
+      }
     },
   },
   computed: {
     productType() {
-      console.log(this.item);
       return this.item.variation ? this.item.variation?.node : this.item.product?.node;
     },
     quantity() {
