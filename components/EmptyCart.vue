5fb2ef32b90f6b015b858979045db9749964ab39
Scott Kennedy <scottyzen@gmail.com>
2023-01-28 20:23:21 +0000

    Add trycatch to empty cart

--- components/EmptyCart.vue
+++ components/EmptyCart.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="cursor-pointer top-6 right-8 absolute" @click="EmptyCart" title="Empty Cart">
+  <button class="cursor-pointer top-6 right-8 absolute" @click="emptyCart" title="Empty Cart">
     <TrashIcon />
   </button>
 </template>
@@ -8,10 +8,14 @@
 import EMPTY_CART from "~/gql/mutations/EMPTY_CART";
 export default {
   methods: {
-    async EmptyCart() {
-      const { emptyCart } = await this.$graphql.default.request(EMPTY_CART);
-      if (emptyCart) {
-        this.$store.commit("updateCart", emptyCart.cart);
+    async emptyCart() {
+      try {
+        const { emptyCart } = await this.$graphql.default.request(EMPTY_CART);
+        if (emptyCart) {
+          this.$store.commit("updateCart", emptyCart.cart);
+        }
+      } catch (error) {
+        console.log(error);
       }
     },
   },
