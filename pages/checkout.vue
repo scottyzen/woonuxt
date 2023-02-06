01911c859da08f4c00b12d55b114e876e30d7af7
Scott Kennedy <scottyzen@gmail.com>
2023-02-06 20:29:11 +0000

    HOTFIX: Remvoe __typename from billing and shipping input

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="container flex flex-wrap my-12 gap-8 justify-evenly items-start md:flex-row-reverse lg:gap-12">
-    <!-- <pre>cart: {{cart}}</pre> -->
-    <div class="bg-white border-b rounded-2xl shadow-sm w-full p-6 top-20 text-gray-700 md:max-w-sm md:mt-16 md:sticky">
+  <div
+    class="container flex flex-wrap my-12 gap-8 justify-evenly items-start md:flex-row-reverse lg:gap-12"
+  >
+    <!-- <pre>cart: {{ billing }}</pre> -->
+    <div
+      class="bg-white border-b rounded-2xl shadow-sm w-full p-6 top-20 text-gray-700 md:max-w-sm md:mt-16 md:sticky"
+    >
       <h2 class="font-semibold mb-4">Order Summary</h2>
       <div v-if="cart" class>
         <div class="flex justify-between">
@@ -24,7 +28,10 @@
       </div>
     </div>
 
-    <form @submit.prevent="pay" class="w-full max-w-2xl grid gap-8 checkout-form md:flex-1">
+    <form
+      @submit.prevent="pay"
+      class="w-full max-w-2xl grid gap-8 checkout-form md:flex-1"
+    >
       <div>
         <h2 class="text-xl mb-3 w-full">Billing Details</h2>
         <BillingDetails :billing="billing" @update-billing="updateBilling" />
@@ -32,13 +39,21 @@
 
       <label for="shipToDifferentAddress" class="flex gap-2 items-center">
         <span>Ship to a different address?</span>
-        <input type="checkbox" name="shipToDifferentAddress" id="shipToDifferentAddress" v-model="shipToDifferentAddress" />
+        <input
+          type="checkbox"
+          name="shipToDifferentAddress"
+          id="shipToDifferentAddress"
+          v-model="shipToDifferentAddress"
+        />
       </label>
 
       <transition name="scale-y" mode="out-in">
         <div v-if="shipToDifferentAddress">
           <h2 class="text-xl mb-3 w-full">Shipping Details</h2>
-          <ShippingDetails :shipping="shipping" @update-shipping="updateShipping" />
+          <ShippingDetails
+            :shipping="shipping"
+            @update-shipping="updateShipping"
+          />
         </div>
       </transition>
 
@@ -47,15 +62,33 @@
         <h2 class="text-xl mb-3 w-full">Payment Options</h2>
         <div class="flex gap-4">
           <div class="flex gap-1">
-            <input type="radio" name="paymentMethod" id="cc" value="stripe" v-model="paymentMethod" />
+            <input
+              type="radio"
+              name="paymentMethod"
+              id="cc"
+              value="stripe"
+              v-model="paymentMethod"
+            />
             <label for="cc">Credit or Debit Card</label>
           </div>
           <div class="flex gap-1">
-            <input type="radio" name="paymentMethod" id="cod" value="cod" v-model="paymentMethod" />
+            <input
+              type="radio"
+              name="paymentMethod"
+              id="cod"
+              value="cod"
+              v-model="paymentMethod"
+            />
             <label for="cod">Cash on Delivery</label>
           </div>
           <div class="flex gap-1">
-            <input type="radio" name="paymentMethod" id="paypal" value="paypal" v-model="paymentMethod" />
+            <input
+              type="radio"
+              name="paymentMethod"
+              id="paypal"
+              value="paypal"
+              v-model="paymentMethod"
+            />
             <label for="paypal">PayPal</label>
           </div>
         </div>
@@ -70,18 +103,32 @@
             #default="{ elements }"
             ref="elms"
           >
-            <StripeElement type="card" :elements="elements" :options="cardOptions" ref="card" />
+            <StripeElement
+              type="card"
+              :elements="elements"
+              :options="cardOptions"
+              ref="card"
+            />
           </StripeElements>
-          <span class="mt-1 text-sm text-right text-orange-400 capitalize block">Test mode: 4242 4242 4242 4242</span>
+          <span class="mt-1 text-sm text-right text-orange-400 capitalize block"
+            >Test mode: 4242 4242 4242 4242</span
+          >
         </div>
       </div>
 
       <div>
         <h2 class="text-xl mb-3 w-full">Order Note (Optional)</h2>
-        <textarea name="order-note" id="order-note" class="w-full" v-model="orderNote"></textarea>
+        <textarea
+          name="order-note"
+          id="order-note"
+          class="w-full"
+          v-model="orderNote"
+        ></textarea>
       </div>
 
-      <button class="bg-primary rounded-2xl shadow-md my-4 text-white text-lg text-center mb-8 p-3 block justify-evenly hover:bg-primary-dark">
+      <button
+        class="bg-primary rounded-2xl shadow-md my-4 text-white text-lg text-center mb-8 p-3 block justify-evenly hover:bg-primary-dark"
+      >
         {{ buttonText }}
       </button>
     </form>
@@ -89,35 +136,35 @@
 </template>
 
 <script>
-import { StripeElements, StripeElement } from "vue-stripe-elements-plus";
-import CHECKOUT from "~/gql/mutations/checkout";
+import { StripeElements, StripeElement } from 'vue-stripe-elements-plus';
+import CHECKOUT from '~/gql/mutations/checkout';
 
 export default {
   middleware({ app, store, redirect, dispatch, commit }) {
     if (!store.state.cart || store.state.cart.isEmpty) {
-      return redirect("/");
+      return redirect('/');
     }
   },
   head() {
-    return { title: "Checkout" };
+    return { title: 'Checkout' };
   },
   data() {
     return {
       loadStripe: false,
-      buttonText: "Place Order",
+      buttonText: 'Place Order',
       billing: {
-        country: "IE",
+        country: 'IE',
       },
       shipping: {
-        country: "IE",
+        country: 'IE',
       },
-      orderNote: "",
+      orderNote: '',
       shipToDifferentAddress: false,
-      paymentMethod: "stripe",
+      paymentMethod: 'stripe',
       cardOptions: {
         hidePostalCode: true,
         style: {
-          base: { color: "#1F2937", fontSize: "16px" },
+          base: { color: '#1F2937', fontSize: '16px' },
         },
       },
     };
@@ -128,15 +175,15 @@ export default {
   },
   methods: {
     pay() {
-      this.buttonText = "Processing...";
-      if (this.paymentMethod == "stripe") {
+      this.buttonText = 'Processing...';
+      if (this.paymentMethod == 'stripe') {
         this.payWithStripe();
       } else {
         this.checkout();
       }
     },
     payWithStripe() {
-      this.buttonText = "Processing...";
+      this.buttonText = 'Processing...';
 
       // ref in template
       const groupComponent = this.$refs.elms;
@@ -145,36 +192,53 @@ export default {
       const cardElement = cardComponent.stripeElement;
 
       groupComponent.instance.createSource(cardElement).then((result) => {
-        this.buttonText = "Checking out...";
+        this.buttonText = 'Checking out...';
         this.checkout(result.source.id);
       });
     },
+
     async checkout(sourceId) {
+      const removeTypename = (obj) => {
+        const newObj = {};
+        Object.keys(obj).forEach((key) => {
+          if (key !== '__typename') {
+            newObj[key] = obj[key];
+          }
+        });
+        return newObj;
+      };
       try {
         const variables = {
-          billing: this.billing,
-          shipping: this.shipToDifferentAddress ? this.shipping : this.billing,
-          metaData: sourceId ? [{ key: "_stripe_source_id", value: sourceId }] : null,
+          billing: removeTypename(this.billing),
+          shipping: this.shipToDifferentAddress
+            ? removeTypename(this.shipping)
+            : removeTypename(this.billing),
+          metaData: sourceId
+            ? [{ key: '_stripe_source_id', value: sourceId }]
+            : null,
           paymentMethod: this.paymentMethod,
           customerNote: this.orderNote,
           shipToDifferentAddress: this.shipToDifferentAddress,
         };
 
-        const { checkout } = await this.$graphql.default.request(CHECKOUT, variables);
-        if (checkout.result == "success") {
-          this.buttonText = "Order Successfull";
-          this.$store.commit("updateCart", null);
-          if (this.paymentMethod == "paypal") {
+        const { checkout } = await this.$graphql.default.request(
+          CHECKOUT,
+          variables
+        );
+        if (checkout.result == 'success') {
+          this.buttonText = 'Order Successfull';
+          this.$store.commit('updateCart', null);
+          if (this.paymentMethod == 'paypal') {
             // this.$router.push(checkout.redirect);
             window.location.href = checkout.redirect;
           } else {
             this.$router.push({
-              name: "order-summary",
+              name: 'order-summary',
               params: { order: checkout.order },
             });
           }
         } else {
-          this.buttonText = "Place Order";
+          this.buttonText = 'Place Order';
         }
       } catch (error) {
         console.log(error);
@@ -186,8 +250,8 @@ export default {
         this.loadStripe = true;
       } else {
         // Add the stripe script to the head
-        const script = document.createElement("script");
-        script.src = "https://js.stripe.com/v3/";
+        const script = document.createElement('script');
+        script.src = 'https://js.stripe.com/v3/';
         script.async = true;
         document.head.appendChild(script);
         // Wait for the stripe script to be loaded
@@ -215,9 +279,9 @@ export default {
 </script>
 
 <style lang="postcss">
-.checkout-form input[type="text"],
-.checkout-form input[type="email"],
-.checkout-form input[type="tel"],
+.checkout-form input[type='text'],
+.checkout-form input[type='email'],
+.checkout-form input[type='tel'],
 .checkout-form textarea,
 .checkout-form .StripeElement,
 .checkout-form select {
