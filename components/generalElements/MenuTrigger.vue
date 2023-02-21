77a005114ebe6c88001598dbbf1eb0f89de34a88
Scott Kennedy <scottyzen@gmail.com>
2023-02-21 20:33:46 +0000

    Hide hamburger menu on desktop

--- components/generalElements/MenuTrigger.vue
+++ components/generalElements/MenuTrigger.vue
@@ -1,7 +1,9 @@
 <script setup>
-const { isShowingMobileMenu, toggleMobileMenu } = useHelpers();
+const { toggleMobileMenu } = useHelpers();
 </script>
 
 <template>
-  <Icon name="ion:menu-outline" size="26" class="cursor-pointer -my-1 mr-4" @click="toggleMobileMenu" />
+  <div>
+    <Icon name="ion:menu-outline" size="26" class="cursor-pointer -my-1 mr-4" @click="toggleMobileMenu" />
+  </div>
 </template>
