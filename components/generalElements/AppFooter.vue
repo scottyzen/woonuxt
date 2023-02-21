1e3dc38c363ad44c1f023532da1361e520463d5a
Scott Kennedy <scottyzen@gmail.com>
2023-02-21 19:42:45 +0000

    Add mobile menu and tweaked footer layout

--- components/generalElements/AppFooter.vue
+++ components/generalElements/AppFooter.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="bg-white">
-    <div class="container flex flex-wrap my-24 gap-24">
+    <div class="container flex flex-wrap my-24 gap-12 justify-between md:gap-24">
       <div class="mr-auto">
         <Logo />
-        <p class="max-w-sm mt-4 text-sm text-gray-700">
+        <p class="mt-4 text-sm text-gray-700 xl:max-w-sm">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet aliquam lacinia, nunc odio lacinia tortor, vel
           aliquam nisl lorem quis nunc. Sed euismod, nisl sit amet aliquam lacinia, nunc odio lacinia tortor, vel aliquam nisl lorem quis
           nunc.
         </p>
       </div>
-      <div>
+      <div class="w-3/7 lg:w-auto">
         <div class="font-semibold mb-1">Information</div>
         <div class="text-sm">
           <a href="#" class="py-1.5 block">About Us</a>
@@ -18,7 +18,7 @@
           <a href="#" class="py-1.5 block">Affiliates</a>
         </div>
       </div>
-      <div>
+      <div class="w-3/7 lg:w-auto">
         <div class="font-semibold mb-1">Products</div>
         <div class="text-sm">
           <NuxtLink to="/products?orderby=date&order=DESC" class="py-1.5 block">New Arrivals</NuxtLink>
@@ -27,7 +27,7 @@
           <a href="#" class="py-1.5 block">Gift Cards</a>
         </div>
       </div>
-      <div>
+      <div class="w-3/7 lg:w-auto">
         <div class="font-semibold mb-1">Customer Service</div>
         <div class="text-sm">
           <a href="#" class="py-1.5 block">Contact Us</a>
@@ -36,7 +36,7 @@
           <a href="#" class="py-1.5 block">Terms & Conditions</a>
         </div>
       </div>
-      <div>
+      <div class="w-3/7 lg:w-auto">
         <div class="font-semibold mb-1">My Account</div>
         <div class="text-sm">
           <NuxtLink to="/my-account" class="py-1.5 block">My Account</NuxtLink>
---
+++ components/generalElements/MainMenu.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/products">All Products</NuxtLink>
+    <NuxtLink to="/categories">Categories</NuxtLink>
+    <NuxtLink to="/contact">Contact</NuxtLink>
+  </nav>
+</template>
