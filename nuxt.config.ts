37e96833f6b9330d0ce401216e80e857b774bda3
Scott Kennedy <scottyzen@gmail.com>
2023-03-25 22:05:57 +0000

    Fix images after recent nuxt/image update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
     },
     ipx: {
       dir: 'public',
+      domains: process.env.NUXT_IMAGE_DOMAINS ? process.env.NUXT_IMAGE_DOMAINS.replace(/ /g, '').split(',') : [],
     },
   },
 
