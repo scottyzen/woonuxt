d821186b97b8cf0374a80a670ac8c087701a4da1
Scott Kennedy <scottyzen@gmail.com>
2023-03-25 21:42:11 +0000

    headers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,6 @@ export default defineNuxtConfig({
 
   image: {
     domains: process.env.NUXT_IMAGE_DOMAINS ? process.env.NUXT_IMAGE_DOMAINS.replace(/ /g, '').split(',') : [],
-    dir: 'public',
   },
 
   runtimeConfig: {
