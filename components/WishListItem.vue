7a085c702c820e5ef4f05e41e653baf8320ad5cc
Scott Kennedy <scottyzen@gmail.com>
2023-04-18 20:13:15 +0100

    Fix wishlist images #30. Updated Nuxt to 3.4.1

--- components/WishListItem.vue
+++ components/WishListItem.vue
@@ -2,25 +2,18 @@
   <li class="flex py-4 gap-4 items-center">
     <button title="Remove Item" @click="removeFromList(product.databaseId)">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24">
-        <path
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="32"
-          d="M368 368L144 144M368 144L144 368" />
+        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 368L144 144M368 144L144 368" />
       </svg>
     </button>
     <NuxtLink :to="`/product/${product.slug}`">
-      <NuxtImg
+      <img
         v-if="product.image"
         class="rounded-lg object-cover h-20 w-20"
-        :src="product.image.sourceUrl"
+        :src="product.image.cartSourceUrl || product.image.sourceUrl"
         :alt="product.image.altText || product.name"
         :title="product.image.altText || product.name"
         width="100"
-        height="120"
-        fit="outside" />
+        height="120" />
     </NuxtLink>
     <NuxtLink class="text-lg leading-tight" :to="`/product/${product.slug}`">{{ product.name }}</NuxtLink>
     <ProductPrice :sale-price="product.salePrice" :regular-price="product.regularPrice" class="ml-auto" />
