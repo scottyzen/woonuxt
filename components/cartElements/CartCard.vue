6980cdcc0643375f5ec1256510b66d61f340b54f
Scott Kennedy <scottyzen@gmail.com>
2023-05-15 20:42:49 +0100

    Add product link to cart item

--- components/cartElements/CartCard.vue
+++ components/cartElements/CartCard.vue
@@ -12,30 +12,37 @@ const updateQuantity = () => {
 </script>
 
 <template>
-  <li v-if="item" class="flex gap-4 items-center">
-    <img
-      v-if="productType.image"
-      width="64"
-      height="64"
-      class="rounded-lg h-16 w-16"
-      :src="productType.image.cartSourceUrl"
-      :alt="productType.image.altText || productType.name"
-      :title="productType.image.altText || productType.name"
-      loading="lazy" />
+  <li v-if="item" class="flex items-center gap-4">
+    <NuxtLink :to="props.item.product.node.slug">
+      <img
+        v-if="productType.image"
+        width="64"
+        height="64"
+        class="w-16 h-16 rounded-lg"
+        :src="productType.image.cartSourceUrl"
+        :alt="productType.image.altText || productType.name"
+        :title="productType.image.altText || productType.name"
+        loading="lazy" />
+    </NuxtLink>
     <div class="flex-1">
-      <div class="leading-tight">{{ productType.name }}</div>
-      <ProductPrice
-        class="mt-1 text-xs"
-        :sale-price="productType.salePrice"
-        :regular-price="productType.regularPrice" />
+      <NuxtLink class="leading-tight" :to="props.item.product.node.slug">{{ productType.name }}</NuxtLink>
+      <ProductPrice class="mt-1 text-xs" :sale-price="productType.salePrice" :regular-price="productType.regularPrice" />
     </div>
     <input
       v-model.number="quantity"
       type="number"
       min="0"
       aria-label="Quantity"
-      class="bg-white border rounded-lg flex text-left p-2 w-16 gap-4 items-center justify-center focus:outline-none"
+      class="flex items-center justify-center w-16 gap-4 p-2 text-left bg-white border rounded-lg focus:outline-none"
       :disabled="isUpdatingCart"
       @input="updateQuantity" />
   </li>
 </template>
+
+<style scoped>
+/* alwsys show up and down buttons on number inpout */
+input[type='number']::-webkit-inner-spin-button,
+input[type='number']::-webkit-outer-spin-button {
+  opacity: 1;
+}
+</style>
