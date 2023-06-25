4a4b8a869db1cd6a5f0ff54ebc18d0c783581ac8
Scott Kennedy <scottyzen@gmail.com>
2023-06-26 00:35:52 +0100

    Tweak search in put styles

--- components/shopElements/ProductSearch.vue
+++ components/shopElements/ProductSearch.vue
@@ -23,7 +23,7 @@ onBeforeRouteUpdate((to, from, next) => {
       v-model="searchQuery"
       type="text"
       :placeholder="$t('messages.shop.searchProducts')"
-      class="z-0 inline-flex items-center w-full p-2 pl-10 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md" />
+      class="z-0 inline-flex items-center w-full p-2 pl-10 text-sm font-medium text-gray-500 border border-gray-300 rounded-md shadow-inner outline-none bg-gray-50 shadow-gray-100" />
     <span v-if="searchQuery" class="absolute z-10 flex items-center gap-1 px-2 py-1 text-xs rounded cursor-pointer bg-primary bg-opacity-10 text-primary right-2" @click="reset">
       <span>{{ $t('messages.general.clear') }}</span>
       <Icon name="ion:close-outline" size="18" />
