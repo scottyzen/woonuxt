dd90f6d0edd4128059447f4b73b8e70619dd5ec9
Scott Kennedy <scottyzen@gmail.com>
2023-09-03 22:58:34 +0100

    Changed Tailwind config to TypeScript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+export default <Partial<Config>>{
   content: ['./components/**/*.{js,vue,ts}', './layouts/**/*.vue', './pages/**/*.vue', './plugins/**/*.{js,ts}', './nuxt.config.{js,ts}', './app.vue'],
   theme: {
     container: {
