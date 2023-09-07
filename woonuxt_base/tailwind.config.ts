8aa5b949612caf097ec561f8da40cc0b8f8dba73
Scott Kennedy <scottyzen@gmail.com>
2023-09-08 00:25:40 +0100

    Tailwind config to TS

---
+++ woonuxt_base/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss';
+
+export default <Partial<Config>>{
+  content: ['./components/**/*.{js,vue,ts}', './layouts/**/*.vue', './pages/**/*.vue', './plugins/**/*.{js,ts}', './nuxt.config.{js,ts}', './app.vue'],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      colors: {
+        primary: {
+          light: '#AE7DDD',
+          DEFAULT: process.env.PRIMARY_COLOR || '#7F54B2',
+          dark: '#754fa3',
+        },
+      },
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
