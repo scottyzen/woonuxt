8ef1ee375ff7b4d0403b8a490ec377e18d53f048
Scott Kennedy <scottyzen@gmail.com>
2023-09-07 23:15:17 +0100

    Server capabilities

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@ export default defineNuxtConfig({
   // Get all the pages, components, composeables and plugins from the parent theme
   extends: ['./woonuxt_base'],
 
+  // Depending on your servers capabilities, you may need to adjust the following settings.
+  // It will affect the build time but also increase the reliability of the build process.
+  // If you have a setver with a lot of memory and CPU, you remove the following settings.
+  nitro: {
+    prerender: {
+      concurrency: 10, // How many pages to prerender at once
+      interval: 1000, // How long to wait between prerendering pages
+    },
+  },
+
   // Multilingual support
   i18n: {
     locales: [
