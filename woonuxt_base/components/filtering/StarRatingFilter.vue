857124532992cd6176ce109acf7bc3b006d664be
Scott Kennedy <scottyzen@gmail.com>
2023-10-09 20:06:06 +0100

    Improved StarRatingFilter component

--- woonuxt_base/components/filtering/StarRatingFilter.vue
+++ woonuxt_base/components/filtering/StarRatingFilter.vue
@@ -1,22 +1,23 @@
-<script setup>
+<script setup lang="ts">
 const { getFilter, setFilter, isFiltersActive } = await useFiltering();
-const selectedTerms = ref(getFilter('rating') || []);
 
+const selectedTerms = ref<string[]>(getFilter('rating'));
 const isOpen = ref(true);
 
+/**
+ * @description This watches the isFiltersActive variable and unchecks all radio boxes when filters are cleared.
+ * @example If the user clicks the 'clear filters' button, the isFiltersActive variable would change to false.
+ */
 watch(isFiltersActive, () => {
-  // uncheck all radio boxes when filters are cleared
   if (!isFiltersActive.value) selectedTerms.value = [];
 });
 
-const radioClicked = (e) => {
-  if (selectedTerms.value === parseInt(e.target.value)) {
-    selectedTerms.value = [];
-    setFilter('rating', []);
-  } else {
-    selectedTerms.value = parseInt(e.target.value);
-    setFilter('rating', [e.target.value]);
-  }
+/**
+ * @param {string} rating - This is a string instead of a number because the setFilter function is used globally and it only accepts strings.
+ * @description This sets the filter to the selected rating. If the rating is already selected, it will be removed from the filter.
+ */
+const radioClicked = (rating: string): void => {
+  setFilter('rating', selectedTerms.value.includes(rating) ? [] : [rating]);
 };
 </script>
 
@@ -29,26 +30,24 @@ const radioClicked = (e) => {
     </div>
     <div v-if="isOpen" class="mt-3 text-sm grid text-gray-500 gap-2">
       <div class="cursor-pointer flex gap-2 items-center">
-        <input id="star-five" v-model="selectedTerms" type="radio" :value="5" aria-label="5 stars" @click="radioClicked" />
-        <label for="star-five">
-          <StarRating :rating="5" :size="16" />
-        </label>
+        <input id="star-five" v-model="selectedTerms" type="radio" value="5" aria-label="5 stars" @click="radioClicked('5')" />
+        <label for="star-five"><StarRating :rating="5" :size="16" /></label>
       </div>
       <div class="cursor-pointer flex gap-2 items-center">
-        <input id="star-four" v-model="selectedTerms" type="radio" :value="4" aria-label="4 stars" @click="radioClicked" />
-        <label for="star-four"> <StarRating :rating="4" :size="16" /> & {{ $t('messages.general.up') }} </label>
+        <input id="star-four" v-model="selectedTerms" type="radio" value="4" aria-label="4 stars" @click="radioClicked('4')" />
+        <label for="star-four"><StarRating :rating="4" :size="16" /> & {{ $t('messages.general.up') }}</label>
       </div>
       <div class="cursor-pointer flex gap-2 items-center">
-        <input id="star-three" v-model="selectedTerms" type="radio" :value="3" aria-label="3 stars" @click="radioClicked" />
-        <label for="star-three"> <StarRating :rating="3" :size="16" /> & {{ $t('messages.general.up') }} </label>
+        <input id="star-three" v-model="selectedTerms" type="radio" value="3" aria-label="3 stars" @click="radioClicked('3')" />
+        <label for="star-three"><StarRating :rating="3" :size="16" /> & {{ $t('messages.general.up') }}</label>
       </div>
       <div class="cursor-pointer flex gap-2 items-center">
-        <input id="star-two" v-model="selectedTerms" type="radio" :value="2" aria-label="2 stars" @click="radioClicked" />
-        <label for="star-two"> <StarRating :rating="2" :size="16" /> & {{ $t('messages.general.up') }} </label>
+        <input id="star-two" v-model="selectedTerms" type="radio" value="2" aria-label="2 stars" @click="radioClicked('2')" />
+        <label for="star-two"><StarRating :rating="2" :size="16" /> & {{ $t('messages.general.up') }}</label>
       </div>
       <div class="cursor-pointer flex gap-2 items-center">
-        <input id="star-one" v-model="selectedTerms" type="radio" :value="1" aria-label="1 star" @click="radioClicked" />
-        <label for="star-one"> <StarRating :rating="1" :size="16" /> & {{ $t('messages.general.up') }} </label>
+        <input id="star-one" v-model="selectedTerms" type="radio" value="1" aria-label="1 star" @click="radioClicked('1')" />
+        <label for="star-one"><StarRating :rating="1" :size="16" /> & {{ $t('messages.general.up') }}</label>
       </div>
     </div>
   </div>
