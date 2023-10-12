184636adcd7b5febf99c75e7655a363ee6032038
Scott Kennedy <scottyzen@gmail.com>
2023-10-12 19:50:00 +0100

    ts fixx

--- woonuxt_base/components/WishListItem.vue
+++ woonuxt_base/components/WishListItem.vue
@@ -5,7 +5,7 @@ const { product } = defineProps<{ product: Product }>();
 
 <template>
   <li class="flex py-4 gap-4 items-center">
-    <button title="Remove Item" @click="removeFromWishlist(product.databaseId)">
+    <button v-if="product.databaseId" title="Remove Item" @click="removeFromWishlist(product.databaseId)">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24">
         <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 368L144 144M368 144L144 368" />
       </svg>
