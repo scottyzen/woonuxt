5464efe01f704ed7dde906d8eefb22b5db459925
Scott Kennedy <scottyzen@gmail.com>
2023-10-29 00:38:42 +0100

    Re enambled codegen

--- woonuxt_base/nuxt.config.ts
+++ woonuxt_base/nuxt.config.ts
@@ -17,10 +17,6 @@ export default defineNuxtConfig({
 
   modules: ['woonuxt-settings', 'nuxt-graphql-client', '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/image', '@nuxtjs/i18n'],
 
-  'graphql-client': {
-    codegen: false,
-  },
-
   image: {
     domains: process.env.NUXT_IMAGE_DOMAINS ? process.env.NUXT_IMAGE_DOMAINS.replace(/ /g, '').split(',') : [],
   },
