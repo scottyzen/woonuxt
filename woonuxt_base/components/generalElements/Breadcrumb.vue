3174b372a97125c00b3c9b304403c06edd368768
Scott Kennedy <scottyzen@gmail.com>
2023-10-29 12:27:56 +0000

    Fix breadcrumb

--- woonuxt_base/components/generalElements/Breadcrumb.vue
+++ woonuxt_base/components/generalElements/Breadcrumb.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const runtimeConfig = useRuntimeConfig();
+const { decodeURI } = useHelpers();
 
 const { product } = defineProps<{ product: Product }>();
 
@@ -23,7 +24,7 @@ const format = computed(() => [
       <span> /</span>
     </span>
     <span v-for="(link, i) in format" :key="link.name || i">
-      <NuxtLink v-if="link.slug" :to="link.slug" class="hover:text-primary">{{ link.name }}</NuxtLink>
+      <NuxtLink v-if="link.slug" :to="decodeURI(link.slug)" class="hover:text-primary">{{ link.name }}</NuxtLink>
       <span v-else class="text-gray-800">{{ link.name }}</span>
       <span v-if="i + 1 < format.length"> /</span>
     </span>
