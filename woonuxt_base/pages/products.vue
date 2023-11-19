da13567f9897779bd4dfae5773c39d410e97934c
Scott Kennedy <scottyzen@gmail.com>
2023-11-19 19:27:33 +0000

    Fix filter bug

--- woonuxt_base/pages/products.vue
+++ woonuxt_base/pages/products.vue
@@ -20,7 +20,7 @@ useHead({
   <div class="container flex items-start gap-16">
     <Filters />
 
-    <div class="w-full" v-if="allProducts">
+    <div class="w-full">
       <div class="flex items-center justify-between w-full gap-4 mt-8 md:gap-8">
         <ProductResultCount />
         <OrderByDropdown class="hidden md:inline-flex" />
