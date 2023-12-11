24562ae7ac70a49fff1a4e306e80652828a16ee8
Scott Kennedy <scottyzen@gmail.com>
2023-12-11 23:01:01 +0000

    Update HeroBanner and favicon for Xmas

--- woonuxt_base/components/generalElements/HeroBanner.vue
+++ woonuxt_base/components/generalElements/HeroBanner.vue
@@ -4,7 +4,7 @@
       width="1400"
       height="800"
       class="object-cover w-full h-[420px] lg:h-[560px] xl:h-[640px]"
-      src="/images/hero-4.jpg"
+      src="/images/hero-xmas.jpg"
       alt="Hero image"
       loading="eager"
       format="webp"
@@ -12,13 +12,13 @@
       sizes="sm:100vw lg:1400px"
       fetchpriority="high"
       preload />
-    <div class="container absolute inset-0 flex flex-col items-start justify-center text-gray-800 bg-gradient-to-l from-gray-100 md:bg-none md:p-12 lg:p-24">
+    <div class="container absolute inset-0 flex flex-col items-start justify-center text-gray-100 bg-gradient-to-l from-gray-800 md:bg-none p-8">
       <h1 class="text-3xl font-bold md:mb-4 md:text-4xl lg:text-6xl">Just landed.</h1>
-      <h2 class="text-lg font-bold md:mb-4 lg:text-3xl">The Autumn Collection</h2>
-      <div class="max-w-sm mb-8 text-sm font-light lg:max-w-md">
-        View the Autumn Collection and discover the latest trends in fashion and accessories. We have everything you need to look your best this season.
+      <h2 class="text-lg font-bold md:mb-4 lg:text-3xl">The Christmas Collection</h2>
+      <div class="max-w-sm mb-8 text-md font-light lg:max-w-md">
+        <p>Our Christmas collection is here! We have a wide range of products to make your Christmas extra special.</p>
       </div>
-      <NuxtLink class="px-8 py-4 font-bold text-white bg-gray-800 rounded-xl hover:bg-gray-800" to="/products">Shop now</NuxtLink>
+      <NuxtLink class="px-6 py-3 font-bold text-white bg-gray-800 rounded-xl hover:bg-gray-800" to="/products">Shop now</NuxtLink>
     </div>
   </div>
 </template>
