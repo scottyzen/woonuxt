3c2ff7f72f7d0d10f94de5e9fbff1b5b40d5448f
opestro <mahdiharzallah21@gmail.com>
2023-12-28 23:46:50 +0100

    d

--- woonuxt_base/pages/products.vue
+++ woonuxt_base/pages/products.vue
@@ -3,13 +3,53 @@ const { setProducts, updateProductList } = useProducts();
 const route = useRoute();
 
 const { isQueryEmpty } = useHelpers();
+const pageSize = 25;
+let afterProduct = "0";
+const productsRef = ref(['']);
+ async function  datagrab(afterProductId) {
+   try  {
+    console.log('test 1')
+  const  updatedData  = await useAsyncGql('getProducts' , {after: afterProductId})
 
-const { data } = await useAsyncGql('getProducts', {first:24});
-const allProducts = data.value?.products?.nodes ?? [];
-setProducts(allProducts);
+ const updatedProducts = updatedData.data._value.products.nodes
+  productsRef.value = [...productsRef.value, ...updatedProducts];
+  afterProduct = updatedData.data._value.products.pageInfo.endCursor ?? [];
+  
+  setProducts(productsRef.value);
+  console.log(updatedData)
+  console.log(afterProduct)
+  } catch (e) {console.log('error: ' + e.message)}
+  
+ // datagrab()
+}
+
+
+/*
+.then((data)=>{
+  console.log(data.data)
+  afterProduct = data.data._rawValue.products.pageInfo.endCursor;
+ 
+  return afterProduct;
+
+});
+*/
+
+//let afterProduct = data;
+  // Update products with the new batch
+
+
+  setProducts(productsRef.value);
+
+  // Increment the current page for the next batch
+
+
+//const allProducts = data.value?.products?.nodes ?? [];
+//setProducts(allProducts);
 
 onMounted(() => {
   if (!isQueryEmpty.value) updateProductList();
+  datagrab(afterProduct)
+
 });
 
 watch(
