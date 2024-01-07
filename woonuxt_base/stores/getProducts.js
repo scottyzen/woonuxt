54e82d7c026003ab04637f19db1b66ca6e48e249
opestro <mahdiharzallah21@gmail.com>
2024-01-07 22:54:22 +0100

    Store Done

--- woonuxt_base/stores/getProducts.js
+++ woonuxt_base/stores/getProducts.js
@@ -1,18 +1,26 @@
 export const getProductsStore = defineStore('getProducts', () => {
-    let newProducts = ref([])
-    let topProducts = ref([])
-    let categories = ref([])
-   async function getProductsData() {
-
-        const { data: getTopProducts, pending: pendingLoad } = await useFetch('https://gama.soluve.cloud/products', { params: { 'page': 1, 'orderby': 'popularity' }, });
-        topProducts.value = toRaw(getTopProducts.value)
-        const { data: getCategories } = await useFetch('https://gama.soluve.cloud/categories', { params: { 'per_page': 6 }, })
-        categories.value = getCategories.value
-        const { data: getNewProducts } = await useFetch('https://gama.soluve.cloud/products',  { params: { 'page': 1, 'orderby': 'date' }});
-        newProducts.value = getNewProducts.value
-        console.log(newProducts)
-        console.log(categories)
-        return {topProducts, categories, newProducts}
+    let newProducts = ref()
+    let topProducts = ref()
+    let categories = ref()
+    async function getProductsData() {
+        if (!topProducts.value) {
+         
+            const { data: getTopProducts } = await useFetch('https://gama.soluve.cloud/products', { params: { 'page': 1, 'orderby': 'popularity' }, });
+            topProducts.value = toRaw(getTopProducts.value)
+            
+        }
+        console.log('else')
+        console.log(topProducts)
+        if (!categories.value) {
+            const { data: getCategories } = await useFetch('https://gama.soluve.cloud/categories', { params: { 'per_page': 6 }, })
+            categories.value = getCategories.value
+        }
+        console.log('else')
+        if (!newProducts.value) {
+            const { data: getNewProducts } = await useFetch('https://gama.soluve.cloud/products', { params: { 'page': 1, 'orderby': 'date' } });
+            newProducts.value = getNewProducts.value
+        }
+        console.log('else')
     }
 
     return { newProducts, getProductsData, topProducts, categories }
