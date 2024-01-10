e134d47204bfa44c7eca9b375018c981fed3d447
Scott Kennedy <scottyzen@gmail.com>
2024-01-10 23:50:53 +0000

    Add isDev variable to useHelpers.ts and update package.json version

--- woonuxt_base/components/generalElements/SEOHead.vue
+++ woonuxt_base/components/generalElements/SEOHead.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const { frontEndUrl, wooNuxtSEO } = useHelpers();
+const { frontEndUrl, wooNuxtSEO, isDev } = useHelpers();
 const { path } = useRoute();
 
 const img = useImage();
@@ -9,29 +9,29 @@ const title = info.name;
 const canonical = `${frontEndUrl}${path}`;
 const siteName = process.env.SITE_TITLE || 'WooNuxt';
 
+const imagePrefix = isDev ? '' : frontEndUrl;
+
 const imageURL = info.image.sourceUrl || '/images/placeholder.jpg';
-const defaultImage = img.getSizes(imageURL, { width: 1200, height: 630 }).src;
-const twitterImageSrc = img.getSizes(imageURL, { width: 1600, height: 900 }).src;
+const defaultImage = imagePrefix + img.getSizes(imageURL, { width: 1200, height: 630 }).src;
+const twitterImageSrc = imagePrefix + img.getSizes(imageURL, { width: 1600, height: 900 }).src;
 </script>
 
 <template>
-  <div>
-    <Head>
-      <Title>{{ title }}</Title>
-      <Meta name="description" hid="description" :content="info.description" />
-      <Meta name="image" hid="image" :content="defaultImage" />
-      <Meta property="og:site_name" hid="og:site_name" :content="siteName" />
-      <Meta property="og:url" hid="og:url" :content="canonical" />
-      <Meta property="og:title" hid="og:title" :content="info.name" />
-      <Meta property="og:description" hid="og:description" :content="info.description" />
-      <Meta property="og:image" hid="og:image" :content="defaultImage" />
-      <Meta v-if="wooNuxtSEO.facebook" property="article:publisher" hid="article:publisher" :content="wooNuxtSEO.facebook" />
-      <Meta name="twitter:card" hid="twitter:card" content="summary_large_image" />
-      <Meta name="twitter:title" hid="twitter:title" :content="info.name" />
-      <Meta name="twitter:description" hid="twitter:description" :content="info.description" />
-      <Meta name="twitter:image" hid="twitter:image" :content="twitterImageSrc" />
-      <Meta name="twitter:url" hid="twitter:url" :content="canonical" />
-      <Link rel="canonical" hid="canonical" :href="canonical" />
-    </Head>
-  </div>
+  <Head>
+    <Title>{{ title }}</Title>
+    <Meta name="description" hid="description" :content="info.description" />
+    <Meta name="image" hid="image" :content="defaultImage" />
+    <Meta property="og:site_name" hid="og:site_name" :content="siteName" />
+    <Meta property="og:url" hid="og:url" :content="canonical" />
+    <Meta property="og:title" hid="og:title" :content="info.name" />
+    <Meta property="og:description" hid="og:description" :content="info.description" />
+    <Meta property="og:image" hid="og:image" :content="defaultImage" />
+    <Meta v-if="wooNuxtSEO.facebook" property="article:publisher" hid="article:publisher" :content="wooNuxtSEO.facebook" />
+    <Meta name="twitter:card" hid="twitter:card" content="summary_large_image" />
+    <Meta name="twitter:title" hid="twitter:title" :content="info.name" />
+    <Meta name="twitter:description" hid="twitter:description" :content="info.description" />
+    <Meta name="twitter:image" hid="twitter:image" :content="twitterImageSrc" />
+    <Meta name="twitter:url" hid="twitter:url" :content="canonical" />
+    <Link rel="canonical" hid="canonical" :href="canonical" />
+  </Head>
 </template>
