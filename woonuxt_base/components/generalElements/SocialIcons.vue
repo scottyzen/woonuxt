b6e2b63b775593291cc7f107c12d0dd4f6f861c6
Scott Kennedy <scottyzen@gmail.com>
2024-01-17 19:18:45 +0000

    Add aria-label to social icons

--- woonuxt_base/components/generalElements/SocialIcons.vue
+++ woonuxt_base/components/generalElements/SocialIcons.vue
@@ -4,7 +4,7 @@ const { wooNuxtSEO } = useHelpers();
 
 <template>
   <div v-if="wooNuxtSEO.length" class="flex gap-4 text-xl">
-    <a v-for="item in wooNuxtSEO" :key="item.provider" :href="item.url" target="_blank" rel="noreferrer">
+    <a v-for="item in wooNuxtSEO" :key="item.provider" :href="item.url" target="_blank" rel="noreferrer" :aria-label="item.provider">
       <Icon class="text-gray-700 hover:text-gray-900" :name="`ion:logo-${item.provider}`" />
     </a>
   </div>
