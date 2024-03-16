47a87123b53e47f062edab26155b8cc33bd57819
Scott Kennedy <scottyzen@gmail.com>
2024-03-16 12:29:37 +0000

    Add Stripe payment integration

--- functions/stripe.ts
+++ functions/stripe.ts
@@ -1,13 +1,12 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async (event, context) => {
-  const { email, amount, currency } = JSON.parse(event.body);
-  console.log({ email, amount, currency });
+exports.handler = async (event: any, context: any) => {
+  const { amount, currency } = JSON.parse(event.body);
 
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: parseInt(amount),
-      currency: currency || 'usd',
+      currency: currency || 'eur',
       payment_method_types: ['card'],
     });
 
@@ -16,10 +15,9 @@ exports.handler = async (event, context) => {
       body: JSON.stringify({ ...paymentIntent }),
     };
   } catch (error) {
-    console.log(error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.raw.message }),
+      body: JSON.stringify({ error }),
     };
   }
 };
