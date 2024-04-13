042ea0e45ef5bfe118af751e0c132299bbfa69eb
Scott Kennedy <scottyzen@gmail.com>
2024-04-13 21:54:18 +0100

    Update image directory and format in nuxt.config.ts

--- woonuxt_base/nuxt.config.ts
+++ woonuxt_base/nuxt.config.ts
@@ -34,6 +34,7 @@ export default defineNuxtConfig({
     provider: process.env.IMAGE_PROVIDER || 'ipx',
     domains: process.env.NUXT_IMAGE_DOMAINS ? process.env.NUXT_IMAGE_DOMAINS.replace(/ /g, '').split(',') : [],
     format: ['webp'],
+    dir: resolve('./public'),
   },
 
   hooks: {
