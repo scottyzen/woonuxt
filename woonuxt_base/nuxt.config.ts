38195342d90f800ce2b55e2193aad98d7b3d3be1
Scott Kennedy <scottyzen@gmail.com>
2024-04-21 23:39:59 +0100

    Update image handling and configuration in CartCard.vue and nuxt.config.ts

--- woonuxt_base/nuxt.config.ts
+++ woonuxt_base/nuxt.config.ts
@@ -28,6 +28,7 @@ export default defineNuxtConfig({
 
   image: {
     domains: process.env.NUXT_IMAGE_DOMAINS ? process.env.NUXT_IMAGE_DOMAINS.replace(/ /g, '').split(',') : [],
+    dir: 'public/images',
   },
 
   hooks: {
