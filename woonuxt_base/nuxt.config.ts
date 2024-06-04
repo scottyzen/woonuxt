c01fffdc0e4ca6b2cf18777b16d430e2accb0659
Scott Kennedy <scottyzen@gmail.com>
2024-06-04 12:11:06 +0100

    chore: Refactor Filters.vue to use template tag for attribute loop

--- woonuxt_base/nuxt.config.ts
+++ woonuxt_base/nuxt.config.ts
@@ -47,6 +47,8 @@ export default defineNuxtConfig({
   nitro: {
     routeRules: {
       '/': { prerender: true },
+      '/**': { prerender: true },
+      '/products': { swr: true },
       '/products/**': { swr: 3600 },
       '/checkout/order-received/**': { ssr: false },
       '/order-summary/**': { ssr: false },
