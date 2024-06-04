0a6f87ec73ad5bf01ffa3a1d828541aaf20f9ebe
Scott Kennedy <scottyzen@gmail.com>
2024-06-04 12:57:10 +0100

    chore: Update SWR cache duration for /products route

--- woonuxt_base/nuxt.config.ts
+++ woonuxt_base/nuxt.config.ts
@@ -48,7 +48,7 @@ export default defineNuxtConfig({
     routeRules: {
       '/': { prerender: true },
       '/**': { prerender: true },
-      '/products': { swr: true },
+      '/products': { swr: 3600 },
       '/products/**': { swr: 3600 },
       '/checkout/order-received/**': { ssr: false },
       '/order-summary/**': { ssr: false },
