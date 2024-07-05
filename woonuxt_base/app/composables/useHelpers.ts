436b959c07a1fa5c18deae9a0e7184dffad81c52
Scott Kennedy <scottyzen@gmail.com>
2024-07-05 22:43:09 +0100

    refactor: Add null check for date in formatDate function

--- woonuxt_base/app/composables/useHelpers.ts
+++ woonuxt_base/app/composables/useHelpers.ts
@@ -137,7 +137,10 @@ export function useHelpers() {
    * @param {string} date - The date string to format.
    * @returns {string} The formatted date string.
    */
-  const formatDate = (date: string): string => new Date(date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+  const formatDate = (date?: string | null): string => {
+    if (!date) return '';
+    return new Date(date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+  };
 
   /**
    * Formats a price string.
