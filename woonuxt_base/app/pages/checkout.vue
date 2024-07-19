f13fcd57504744f105dba11b8b512bc46fd9f4ad
Scott Kennedy <scottyzen@gmail.com>
2024-07-19 21:27:07 +0100

    refactor: Tweak styling on no products found and empty cart

--- woonuxt_base/app/pages/checkout.vue
+++ woonuxt_base/app/pages/checkout.vue
@@ -71,13 +71,14 @@ useSeoMeta({
     <LoadingIcon v-if="!cart" class="m-auto" />
     <template v-else>
       <div v-if="cart.isEmpty" class="flex flex-col items-center justify-center flex-1 mb-12">
-        <div class="mb-4 text-xl text-gray-300">{{ $t('messages.shop.cartEmpty') }}</div>
-        
-        <span class="text-gray-400">{{ $t('messages.shop.addProductsInYourCart') }}</span>
-        <NuxtLink to="/products" class="flex items-center justify-center gap-3 p-3 mt-4 font-semibold text-center text-white rounded-lg shadow-md bg-primary hover:bg-primary-dark">
+        <Icon name="ion:cart-outline" size="156" class="opacity-25 mb-5" />
+        <h2 class="text-2xl font-bold mb-2">{{ $t('messages.shop.cartEmpty') }}</h2>
+        <span class="text-gray-400 mb-4">{{ $t('messages.shop.addProductsInYourCart') }}</span>
+        <NuxtLink
+          to="/products"
+          class="flex items-center justify-center gap-3 p-2 px-3 mt-4 font-semibold text-center text-white rounded-lg shadow-md bg-primary hover:bg-primary-dark">
           {{ $t('messages.shop.browseOurProducts') }}
         </NuxtLink>
-
       </div>
 
       <form v-else class="container flex flex-wrap items-start gap-8 my-16 justify-evenly lg:gap-20" @submit.prevent="payNow">
