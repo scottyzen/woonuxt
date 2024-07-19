18d1e6173f8cb1a56480a52530fb9991d7ee55d3
Alex Lykesas <alexookah@me.com>
2024-07-19 18:13:56 +0300

    Fix styles in checkout empty cart

--- woonuxt_base/app/pages/checkout.vue
+++ woonuxt_base/app/pages/checkout.vue
@@ -71,9 +71,9 @@ useSeoMeta({
     <LoadingIcon v-if="!cart" class="m-auto" />
     <template v-else>
       <div v-if="cart.isEmpty" class="flex flex-col items-center justify-center flex-1 mb-12">
-        <div class="mb-4 text-xl text-gray-300">{{ $t('messages.shop.cartEmpty') }}</div>
+        <div class="mb-4 text-xl text-gray-500">{{ $t('messages.shop.cartEmpty') }}</div>
         
-        <span class="text-gray-400">{{ $t('messages.shop.addProductsInYourCart') }}</span>
+        <span class="text-gray-400 pb-2">{{ $t('messages.shop.addProductsInYourCart') }}</span>
         <NuxtLink to="/products" class="flex items-center justify-center gap-3 p-3 mt-4 font-semibold text-center text-white rounded-lg shadow-md bg-primary hover:bg-primary-dark">
           {{ $t('messages.shop.browseOurProducts') }}
         </NuxtLink>
