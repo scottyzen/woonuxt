81431a57100e8c7ce664a33c5a6177c2809b219d
Alex Lykesas <alexookah@me.com>
2024-07-20 05:52:40 +0300

    Redirect logged in users wishlist

--- woonuxt_base/app/pages/wishlist.vue
+++ woonuxt_base/app/pages/wishlist.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+const { viewer } = useAuth();
+const router = useRouter();
+if (viewer.value) router.push(`my-account?tab=wishlist`);
+</script>
+
 <template>
   <WishList />
 </template>
