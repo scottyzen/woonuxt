33cbc93ed11d4f6b6b9df34489f41427a05debac
Alex Lykesas <alexookah@me.com>
2024-08-08 02:49:42 +0300

    Fix form for ids

--- woonuxt_base/app/components/forms/ShippingDetails.vue
+++ woonuxt_base/app/components/forms/ShippingDetails.vue
@@ -12,47 +12,47 @@ const shipping = toRef(props, 'modelValue');
   <div class="grid w-full gap-4 lg:grid-cols-2">
     <div class="w-full">
       <label for="first-name">{{ $t('messages.billing.firstName') }}</label>
-      <input v-model="shipping.firstName" placeholder="John" autocomplete="given-name" type="text" required />
+      <input id="first-name" v-model="shipping.firstName" placeholder="John" autocomplete="given-name" type="text" required />
     </div>
 
     <div class="w-full">
       <label for="last-name">{{ $t('messages.billing.lastName') }}</label>
-      <input v-model="shipping.lastName" placeholder="Doe" autocomplete="family-name" type="text" required />
+      <input id="last-name" v-model="shipping.lastName" placeholder="Doe" autocomplete="family-name" type="text" required />
     </div>
 
     <div class="w-full col-span-full">
       <label for="address1">{{ $t('messages.billing.address1') }}</label>
-      <input v-model="shipping.address1" placeholder="O'Connell Street 47" autocomplete="street-address" type="text" required />
+      <input id="address1" v-model="shipping.address1" placeholder="O'Connell Street 47" autocomplete="street-address" type="text" required />
     </div>
 
     <div class="w-full col-span-full">
       <label for="address2">{{ $t('messages.billing.address2') }} ({{ $t('messages.general.optional') }})</label>
-      <input v-model="shipping.address2" placeholder="Apartment, studio, or floor" autocomplete="address-line2" type="text" />
+      <input id="address2" v-model="shipping.address2" placeholder="Apartment, studio, or floor" autocomplete="address-line2" type="text" />
     </div>
 
     <div class="w-full">
       <label for="city">{{ $t('messages.billing.city') }}</label>
-      <input v-model="shipping.city" placeholder="New York" autocomplete="locality" type="text" required />
+      <input id="city" v-model="shipping.city" placeholder="New York" autocomplete="locality" type="text" required />
     </div>
 
     <div class="w-full">
       <label for="state">{{ $t('messages.billing.state') }} ({{ $t('messages.general.optional') }})</label>
-      <StateSelect v-model="shipping.state" :default-value="shipping.state" :country-code="shipping.country" @change="updateShippingLocation" />
+      <StateSelect id="state" v-model="shipping.state" :default-value="shipping.state" :country-code="shipping.country" @change="updateShippingLocation" />
     </div>
 
     <div class="w-full">
       <label for="country">{{ $t('messages.billing.country') }}</label>
-      <CountrySelect v-model="shipping.country" :default-value="shipping.country" @change="updateShippingLocation" />
+      <CountrySelect id="country" v-model="shipping.country" :default-value="shipping.country" @change="updateShippingLocation" />
     </div>
 
     <div class="w-full">
       <label for="zip">{{ $t('messages.billing.zip') }}</label>
-      <input v-model="shipping.postcode" placeholder="10001" autocomplete="postal-code" type="text" required />
+      <input id="zip" v-model="shipping.postcode" placeholder="10001" autocomplete="postal-code" type="text" required />
     </div>
 
     <div class="w-full col-span-full">
       <label for="phone">{{ $t('messages.billing.phone') }} ({{ $t('messages.general.optional') }})</label>
-      <input v-model="shipping.phone" placeholder="+1 234 567 8901" autocomplete="tel" type="tel" />
+      <input id="phone" v-model="shipping.phone" placeholder="+1 234 567 8901" autocomplete="tel" type="tel" />
     </div>
   </div>
 </template>
