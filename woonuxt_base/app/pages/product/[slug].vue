2bc45441512112dd3fd577852d8f7cb828ea918a
tzhf <theo.zahaf@gmail.com>
2024-08-14 15:31:01 +0200

    handle-product-not-found

--- woonuxt_base/app/pages/product/[slug].vue
+++ woonuxt_base/app/pages/product/[slug].vue
@@ -5,6 +5,7 @@ const route = useRoute();
 const { storeSettings } = useAppConfig();
 const { arraysEqual, formatArray, checkForVariationTypeOfAny } = useHelpers();
 const { addToCart, isUpdatingCart } = useCart();
+const { t } = useI18n();
 const slug = route.params.slug as string;
 
 const { data } = (await useAsyncGql('getProduct', { slug })) as { data: { value: { product: Product } } };
@@ -39,7 +40,7 @@ onMounted(async () => {
     const errorMessage = error?.gqlErrors?.[0].message;
     if (errorMessage) console.error(errorMessage);
   }
-  if (product.value.variations) indexOfTypeAny.value.push(...checkForVariationTypeOfAny(product.value));
+  if (product.value?.variations) indexOfTypeAny.value.push(...checkForVariationTypeOfAny(product.value));
 });
 
 const updateSelectedVariations = (variations: Attribute[]): void => {
@@ -72,97 +73,103 @@ const disabledAddToCart = computed(() => {
 </script>
 
 <template>
-  <main class="container relative py-6 xl:max-w-7xl" v-if="product">
-    <SEOHead :info="product" />
-    <Breadcrumb :product class="mb-6" v-if="storeSettings.showBreadcrumbOnSingleProduct" />
+  <main class="container relative py-6 xl:max-w-7xl">
+    <div v-if="product">
+      <SEOHead :info="product" />
+      <Breadcrumb :product class="mb-6" v-if="storeSettings.showBreadcrumbOnSingleProduct" />
 
-    <div class="flex flex-col gap-10 md:flex-row md:justify-between lg:gap-24">
-      <ProductImageGallery
-        v-if="product.image"
-        class="relative flex-1"
-        :main-image="product.image"
-        :gallery="product.galleryImages!"
-        :node="type"
-        :activeVariation="activeVariation || {}" />
-      <NuxtImg v-else class="relative flex-1 skeleton" src="/images/placeholder.jpg" :alt="product?.name || 'Product'" />
+      <div class="flex flex-col gap-10 md:flex-row md:justify-between lg:gap-24">
+        <ProductImageGallery
+          v-if="product.image"
+          class="relative flex-1"
+          :main-image="product.image"
+          :gallery="product.galleryImages!"
+          :node="type"
+          :activeVariation="activeVariation || {}" />
+        <NuxtImg v-else class="relative flex-1 skeleton" src="/images/placeholder.jpg" :alt="product?.name || 'Product'" />
 
-      <div class="lg:max-w-md xl:max-w-lg md:py-2 w-full">
-        <div class="flex justify-between mb-4">
-          <div class="flex-1">
-            <h1 class="flex flex-wrap items-center gap-2 mb-2 text-2xl font-sesmibold">
-              {{ type.name }}
-              <WPAdminLink :link="`/wp-admin/post.php?post=${product.databaseId}&action=edit`">Edit</WPAdminLink>
-            </h1>
-            <StarRating :rating="product.averageRating || 0" :count="product.reviewCount || 0" v-if="storeSettings.showReviews" />
+        <div class="lg:max-w-md xl:max-w-lg md:py-2 w-full">
+          <div class="flex justify-between mb-4">
+            <div class="flex-1">
+              <h1 class="flex flex-wrap items-center gap-2 mb-2 text-2xl font-sesmibold">
+                {{ type.name }}
+                <WPAdminLink :link="`/wp-admin/post.php?post=${product.databaseId}&action=edit`">Edit</WPAdminLink>
+              </h1>
+              <StarRating :rating="product.averageRating || 0" :count="product.reviewCount || 0" v-if="storeSettings.showReviews" />
+            </div>
+            <ProductPrice class="text-xl" :sale-price="type.salePrice" :regular-price="type.regularPrice" />
           </div>
-          <ProductPrice class="text-xl" :sale-price="type.salePrice" :regular-price="type.regularPrice" />
-        </div>
 
-        <div class="grid gap-2 my-8 text-sm">
-          <div class="flex items-center gap-2">
-            <span class="text-gray-400">{{ $t('messages.shop.availability') }}: </span>
-            <StockStatus :stockStatus @updated="mergeLiveStockStatus" />
-          </div>
-          <div class="flex items-center gap-2" v-if="storeSettings.showSKU">
-            <span class="text-gray-400">{{ $t('messages.shop.sku') }}: </span>
-            <span>{{ product.sku || 'N/A' }}</span>
-          </div>
-        </div>
-
-        <div class="mb-8 font-light prose" v-html="product.shortDescription || product.description" />
-
-        <hr />
-
-        <form @submit.prevent="addToCart(selectProductInput)">
-          <AttributeSelections
-            v-if="product.type == 'VARIABLE' && product.attributes && product.variations"
-            class="mt-4 mb-8"
-            :attributes="product.attributes.nodes"
-            :defaultAttributes="product.defaultAttributes"
-            :variations="product.variations.nodes"
-            @attrs-changed="updateSelectedVariations" />
-          <div class="fixed bottom-0 left-0 z-10 flex items-center w-full gap-4 p-4 mt-12 bg-white md:static md:bg-transparent bg-opacity-90 md:p-0">
-            <input
-              v-model="quantity"
-              type="number"
-              min="1"
-              aria-label="Quantity"
-              class="bg-white border rounded-lg flex text-left p-2.5 w-20 gap-4 items-center justify-center focus:outline-none" />
-            <AddToCartButton class="flex-1 w-full md:max-w-xs" :disabled="disabledAddToCart" :class="{ loading: isUpdatingCart }" />
-          </div>
-        </form>
-
-        <div v-if="storeSettings.showProductCategoriesOnSingleProduct && product.productCategories">
           <div class="grid gap-2 my-8 text-sm">
             <div class="flex items-center gap-2">
-              <span class="text-gray-400">{{ $t('messages.shop.category', 2) }}:</span>
-              <div class="product-categories">
-                <NuxtLink
-                  v-for="category in product.productCategories.nodes"
-                  :key="category.slug"
-                  :to="`/product-category/${decodeURIComponent(category.slug)}`"
-                  class="hover:text-primary"
-                  :title="category.name"
-                  >{{ category.name }}<span class="comma">, </span>
-                </NuxtLink>
-              </div>
+              <span class="text-gray-400">{{ $t('messages.shop.availability') }}: </span>
+              <StockStatus :stockStatus @updated="mergeLiveStockStatus" />
+            </div>
+            <div class="flex items-center gap-2" v-if="storeSettings.showSKU">
+              <span class="text-gray-400">{{ $t('messages.shop.sku') }}: </span>
+              <span>{{ product.sku || 'N/A' }}</span>
             </div>
           </div>
-          <hr />
-        </div>
 
-        <div class="flex flex-wrap gap-4">
-          <WishlistButton :product />
-          <ShareButton :product />
+          <div class="mb-8 font-light prose" v-html="product.shortDescription || product.description" />
+
+          <hr />
+
+          <form @submit.prevent="addToCart(selectProductInput)">
+            <AttributeSelections
+              v-if="product.type == 'VARIABLE' && product.attributes && product.variations"
+              class="mt-4 mb-8"
+              :attributes="product.attributes.nodes"
+              :defaultAttributes="product.defaultAttributes"
+              :variations="product.variations.nodes"
+              @attrs-changed="updateSelectedVariations" />
+            <div class="fixed bottom-0 left-0 z-10 flex items-center w-full gap-4 p-4 mt-12 bg-white md:static md:bg-transparent bg-opacity-90 md:p-0">
+              <input
+                v-model="quantity"
+                type="number"
+                min="1"
+                aria-label="Quantity"
+                class="bg-white border rounded-lg flex text-left p-2.5 w-20 gap-4 items-center justify-center focus:outline-none" />
+              <AddToCartButton class="flex-1 w-full md:max-w-xs" :disabled="disabledAddToCart" :class="{ loading: isUpdatingCart }" />
+            </div>
+          </form>
+
+          <div v-if="storeSettings.showProductCategoriesOnSingleProduct && product.productCategories">
+            <div class="grid gap-2 my-8 text-sm">
+              <div class="flex items-center gap-2">
+                <span class="text-gray-400">{{ $t('messages.shop.category', 2) }}:</span>
+                <div class="product-categories">
+                  <NuxtLink
+                    v-for="category in product.productCategories.nodes"
+                    :key="category.slug"
+                    :to="`/product-category/${decodeURIComponent(category.slug)}`"
+                    class="hover:text-primary"
+                    :title="category.name"
+                    >{{ category.name }}<span class="comma">, </span>
+                  </NuxtLink>
+                </div>
+              </div>
+            </div>
+            <hr />
+          </div>
+
+          <div class="flex flex-wrap gap-4">
+            <WishlistButton :product />
+            <ShareButton :product />
+          </div>
         </div>
       </div>
+      <div v-if="product.description || product.reviews" class="my-32">
+        <ProductTabs :product />
+      </div>
+      <div class="my-32" v-if="product.related && storeSettings.showRelatedProducts">
+        <div class="mb-4 text-xl font-semibold">{{ $t('messages.shop.youMayLike') }}</div>
+        <ProductRow :products="product.related.nodes" class="grid-cols-2 md:grid-cols-4 lg:grid-cols-5" />
+      </div>
     </div>
-    <div v-if="product.description || product.reviews" class="my-32">
-      <ProductTabs :product />
-    </div>
-    <div class="my-32" v-if="product.related && storeSettings.showRelatedProducts">
-      <div class="mb-4 text-xl font-semibold">{{ $t('messages.shop.youMayLike') }}</div>
-      <ProductRow :products="product.related.nodes" class="grid-cols-2 md:grid-cols-4 lg:grid-cols-5" />
+
+    <div class="flex items-center justify-center min-h-[500px]" v-else>
+      <h1 class="text-xl font-bold">{{ t('messages.shop.productNotFound') }}</h1>
     </div>
   </main>
 </template>
