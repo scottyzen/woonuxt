a0abad1f433993e67eadf50185f09dff168ab98d
Scott Kennedy <scottyzen@gmail.com>
2025-02-03 02:36:27 +0000

    feat: add class to NuxtLink for improved styling

--- woonuxt_base/app/components/generalElements/Logo.vue
+++ woonuxt_base/app/components/generalElements/Logo.vue
@@ -8,7 +8,7 @@ const faviconUrl = '/logo.svg';
 </script>
 
 <template>
-  <NuxtLink to="/">
+  <NuxtLink to="/" class="inline-flex items-center gap-2">
     <img v-if="logoUrl" :src="logoUrl" alt="Logo" class="object-contain h-10" />
     <div v-else class="flex items-center gap-2 text-lg font-bold">
       <img :src="faviconUrl" alt="Logo" width="32" height="32" />
