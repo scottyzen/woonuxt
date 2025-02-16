5b59fb2834c652d3e889f2500c97c2f474aa2c33
Alex Lykesas <alexookah@me.com>
2025-02-16 15:03:38 +0200

    Use onMounted instead of onBeforeMounted

--- woonuxt_base/app/components/productElements/AttributeSelections.vue
+++ woonuxt_base/app/components/productElements/AttributeSelections.vue
@@ -43,7 +43,7 @@ const setDefaultAttributes = () => {
 
 const className = (name: string) => `name-${name.toLowerCase()}`;
 
-onBeforeMount(() => {
+onMounted(() => {
   setDefaultAttributes();
   updateAttrs();
 });
