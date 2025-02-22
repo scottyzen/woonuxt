d436a72c732388e480f7d97a2363b15f398dbbc9
wavarr <114770649+wavarr@users.noreply.github.com>
2025-02-22 14:57:03 -0500

    mobile menu update

--- woonuxt_base/app/components/generalElements/MobileMenu.vue
+++ woonuxt_base/app/components/generalElements/MobileMenu.vue
@@ -8,7 +8,7 @@ const { toggleMobileMenu, wooNuxtVersionInfo } = useHelpers();
     <div class="mt-8 text-center">{{ $t('messages.general.menu') }}</div>
     <MainMenu class="m-4 grid p-4 text-gray-500 gap-6" />
     <div class="mt-auto text-center p-8 text-[10px] text-gray-400">
-      <a href="/" :title="wooNuxtVersionInfo">WooNuxt v{{ wooNuxtVersionInfo }}</a>
+      <a href="/" :title="wooNuxtVersionInfo">ModaPrime USA v{{ wooNuxtVersionInfo }}</a>
     </div>
   </div>
 </template>
