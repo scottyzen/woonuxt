ff47b272d05c9cc2c52362b1a1b85b60e699fac9
wavarr <114770649+wavarr@users.noreply.github.com>
2025-02-25 12:35:00 -0500

    refactor: simplify products page with streamlined data fetching and state management

--- woonuxt_base/app/pages/products.vue
+++ woonuxt_base/app/pages/products.vue
@@ -1,42 +1,13 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useProducts } from '../stores/products';
-import { useRoute } from 'vue-router';
-import { useAppConfig } from '../stores/appConfig';
-import { useHelpers } from '../composables/helpers';
-import { useAsyncGql } from '../composables/useAsyncGql';
-
+import { useProducts } from '~/composables/useProducts';
 const { setProducts, updateProductList } = useProducts();
 const route = useRoute();
 const { storeSettings } = useAppConfig();
 const { isQueryEmpty } = useHelpers();
 
-const { orderby, order } = route.query;
-
-const filter = computed(() => {
-  const filterParams = {};
-  Object.entries(route.query).forEach(([key, value]) => {
-    if (key.startsWith('filter_')) {
-      const filterKey = key.replace('filter_', '');
-      filterParams[filterKey] = value;
-    }
-  });
-  return filterParams;
-});
-
-const { data: productData } = await useAsyncGql('getProducts', { 
-  first: 24, 
-  orderby: orderby || 'DATE',
-  order: order || 'DESC',
-  where: { 
-    ...filter.value
-  } 
-});
-
-const products = computed(() => productData.value?.products?.nodes || []);
-const pageInfo = computed(() => productData.value?.products?.pageInfo || {});
-
-setProducts(products);
+const { data } = await useAsyncGql('getProducts');
+const allProducts = (data.value?.products?.nodes || []) as Product[];
+setProducts(allProducts);
 
 onMounted(() => {
   if (!isQueryEmpty.value) updateProductList();
@@ -57,7 +28,7 @@ useHead({
 </script>
 
 <template>
-  <div class="container flex items-start gap-16" v-if="products.length">
+  <div class="container flex items-start gap-16" v-if="allProducts.length">
     <Filters v-if="storeSettings.showFilters" />
 
     <div class="w-full">
