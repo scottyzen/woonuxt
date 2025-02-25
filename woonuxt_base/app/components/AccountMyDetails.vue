ac213a6bf1cb531e46f6f18b35317e29d26dccfe
wavarr <114770649+wavarr@users.noreply.github.com>
2025-02-24 22:20:04 -0500

    STRIPE STRAIGHT STRIPPED SON!!

--- woonuxt_base/app/components/AccountMyDetails.vue
+++ woonuxt_base/app/components/AccountMyDetails.vue
@@ -12,7 +12,6 @@
 .account-form input[type='tel'],
 .account-form input[type='password'],
 .account-form textarea,
-.account-form .StripeElement,
 .account-form select {
   @apply bg-white border rounded-md outline-none w-full py-2 px-4 block md:bg-gray-50;
 }
--- woonuxt_base/app/components/shopElements/PaymentOptions.vue
+++ woonuxt_base/app/components/shopElements/PaymentOptions.vue
@@ -27,8 +27,8 @@ onMounted(() => {
       :class="{ 'active-option': gateway.id === activePaymentMethod.id }"
       @click="updatePaymentMethod(gateway)"
       :title="gateway?.description || gateway?.title || 'Payment Method'">
-      <icon v-if="gateway.id === 'stripe'" name="ion:card-outline" size="20" />
-      <icon v-else-if="gateway.id === 'paypal'" name="ion:logo-paypal" size="20" />
+      <icon v-if="gateway.id === 'paypal'" name="ion:logo-paypal" size="20" />
+      <icon v-else-if="gateway.id === 'btcpay'" name="ion:logo-bitcoin" size="20" />
       <icon v-else name="ion:cash-outline" size="20" />
       <span class="whitespace-nowrap" v-html="gateway.title" />
       <icon name="ion:checkmark-circle" size="20" class="ml-auto text-primary checkmark opacity-0" />
