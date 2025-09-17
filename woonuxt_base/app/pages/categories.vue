421cf6277d26cbeddea3b3ebfe389c5b10be2414
Scott Kennedy <scottyzen@gmail.com>
2025-09-17 01:26:25 +0100

    fix: add fallback message for empty product categories

--- woonuxt_base/app/pages/categories.vue
+++ woonuxt_base/app/pages/categories.vue
@@ -14,5 +14,6 @@ useHead({
     <div v-if="productCategories?.length" class="grid grid-cols-2 gap-4 my-6 md:grid-cols-3 lg:gap-8 xl:grid-cols-4">
       <CategoryCard v-for="(category, i) in productCategories" :key="i" :node="category" :image-loading="i <= 2 ? 'eager' : 'lazy'" />
     </div>
+    <div v-else class="my-6 text-center text-gray-500">No categories found.</div>
   </main>
 </template>
