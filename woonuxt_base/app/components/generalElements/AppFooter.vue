2a7bd06106cfa9e618fe5b93d514d901f8dfbd21
Scott Kennedy <scottyzen@gmail.com>
2025-09-17 01:46:48 +0100

    fix: add fallback links for product navigation in AppFooter component

--- woonuxt_base/app/components/generalElements/AppFooter.vue
+++ woonuxt_base/app/components/generalElements/AppFooter.vue
@@ -25,16 +25,36 @@ const { wishlistLink } = useAuth();
       <div class="w-3/7 lg:w-auto">
         <div class="mb-1 font-semibold">Products</div>
         <div class="text-sm">
-          <NuxtLink to="/products" class="py-1.5 block">{{ $t('messages.shop.newArrivals') }}</NuxtLink>
-          <NuxtLink to="/products?filter=sale[true]" class="py-1.5 block">On sale</NuxtLink>
-          <NuxtLink to="/products?orderby=rating&order=ASC&filter=rating[1]" class="py-1.5 block">Top rated</NuxtLink>
+          <ClientOnly>
+            <NuxtLink to="/products" class="py-1.5 block">{{ $t('messages.shop.newArrivals') }}</NuxtLink>
+            <template #fallback>
+              <a href="/products" class="py-1.5 block">{{ $t('messages.shop.newArrivals') }}</a>
+            </template>
+          </ClientOnly>
+          <ClientOnly>
+            <NuxtLink to="/products?filter=sale[true]" class="py-1.5 block">On sale</NuxtLink>
+            <template #fallback>
+              <a href="/products?filter=sale[true]" class="py-1.5 block">On sale</a>
+            </template>
+          </ClientOnly>
+          <ClientOnly>
+            <NuxtLink to="/products?orderby=rating&order=ASC&filter=rating[1]" class="py-1.5 block">Top rated</NuxtLink>
+            <template #fallback>
+              <a href="/products?orderby=rating&order=ASC&filter=rating[1]" class="py-1.5 block">Top rated</a>
+            </template>
+          </ClientOnly>
           <a href="/" class="py-1.5 block">{{ $t('messages.shop.giftCards') }}</a>
         </div>
       </div>
       <div class="w-3/7 lg:w-auto">
         <div class="mb-1 font-semibold">{{ $t('messages.general.customerService') }}</div>
         <div class="text-sm">
-          <NuxtLink to="/contact" class="py-1.5 block">Contact Us</NuxtLink>
+          <ClientOnly>
+            <NuxtLink to="/contact" class="py-1.5 block">Contact Us</NuxtLink>
+            <template #fallback>
+              <a href="/contact" class="py-1.5 block">Contact Us</a>
+            </template>
+          </ClientOnly>
           <a href="/" class="py-1.5 block">Shipping & Returns</a>
           <a href="/" class="py-1.5 block">Privacy Policy</a>
           <a href="/" class="py-1.5 block">Terms & Conditions</a>
