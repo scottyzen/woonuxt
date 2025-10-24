d174510382556a0d0abc0531b7dcebb4d9090b52
beljp <jplinstra@gmail.com>
2025-10-24 09:50:47 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,27 @@
+// nuxt.config.ts
 export default defineNuxtConfig({
-
-  // Get all the pages, components, composables and plugins from the parent theme
+  // Erft alles uit je base theme
   extends: ['./woonuxt_base'],
 
+  // Auto-import eigen componenten
   components: [{ path: './components', pathPrefix: false }],
 
-  /**
-   * Depending on your servers capabilities, you may need to adjust the following settings.
-   * It will affect the build time but also increase the reliability of the build process.
-   * If you have a server with a lot of memory and CPU, you can remove the following settings.
-   * @property {number} concurrency - How many pages to prerender at once
-   * @property {number} interval - How long to wait between prerendering pages
-   * @property {boolean} failOnError - This stops the build from failing but the page will not be statically generated
-   */
+  // Modules
+  modules: ['@nuxt/image'],
+
+  // Afbeeldingen: GEEN Netlify CDN, gebruik directe URLs
+  image: {
+    provider: 'static', // <-- zet op 'ipx' als je Nuxt-optimalisatie wilt zonder Netlify
+    // Bij 'static' zijn domains/providers niet nodig
+  },
+
+  // Build / prerender instellingen
   nitro: {
     prerender: {
       concurrency: 10,
       interval: 1000,
-      failOnError: false,
+      failOnError: false
     },
     minify: true
-  },
-});
+  }
+})
