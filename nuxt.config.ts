113aa50872293077b14a496ce58dba3eecbd28b4
beljp <jplinstra@gmail.com>
2025-10-24 10:38:20 +0200

    Update nuxt.config.ts
    cdn weg gehaald.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,19 @@
 export default defineNuxtConfig({
+  modules: [
+    // haal de image module weg als je die niet meer wilt gebruiken
+    // '@nuxt/image',
+  ],
 
-  // Get all the pages, components, composables and plugins from the parent theme
-  extends: ['./woonuxt_base'],
-
-  components: [{ path: './components', pathPrefix: false }],
-
-  /**
-   * Depending on your servers capabilities, you may need to adjust the following settings.
-   * It will affect the build time but also increase the reliability of the build process.
-   * If you have a server with a lot of memory and CPU, you can remove the following settings.
-   * @property {number} concurrency - How many pages to prerender at once
-   * @property {number} interval - How long to wait between prerendering pages
-   * @property {boolean} failOnError - This stops the build from failing but the page will not be statically generated
-   */
-  nitro: {
-    prerender: {
-      concurrency: 10,
-      interval: 1000,
-      failOnError: false,
-    },
-    minify: true
-  },
+  image: {
+    // forceer dat de image module géén externe optimalisatie doet
+    provider: 'static',
+    // optioneel: leeg de screens/configs
+    screens: {},
+    // of: zet alle provider settings uit
+    providers: {
+      static: {
+        // geen baseURL instellen
+      }
+    }
+  }
 });
