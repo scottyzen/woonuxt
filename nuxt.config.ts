65f81ba6f0f0a90d499e5c6022a48bf1890d0896
beljp <jplinstra@gmail.com>
2025-10-24 10:27:40 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,9 @@
-// nuxt.config.ts
 export default defineNuxtConfig({
+
   // Get all the pages, components, composables and plugins from the parent theme
   extends: ['./woonuxt_base'],
 
   components: [{ path: './components', pathPrefix: false }],
-
-  // Gebruik @nuxt/image, maar zonder Netlify-proxy
-  modules: ['@nuxt/image'],
-
-  // ⛔ Geen Netlify Image CDN: gebruik directe image-URL's
-  image: {
-    provider: 'static'
-  },
-
-  // Maak GQL_HOST beschikbaar voor je app (voor o.a. vertalingen/menus etc.)
-  // Zet dezelfde env var ook in Netlify → Site settings → Environment variables
-  runtimeConfig: {
-    public: {
-      // Kies precies wat jouw theme verwacht:
-      // 1) Alleen host:
-      GQL_HOST: process.env.GQL_HOST || 'https://wp.kledingzoeken.nl'
-      // of, als jouw theme de volledige endpoint wil:
-      // GQL_HOST: process.env.GQL_HOST || 'https://wp.kledingzoeken.nl/graphql'
-    }
-  },
 
   /**
    * Depending on your servers capabilities, you may need to adjust the following settings.
@@ -37,8 +17,8 @@ export default defineNuxtConfig({
     prerender: {
       concurrency: 10,
       interval: 1000,
-      failOnError: false
+      failOnError: false,
     },
     minify: true
-  }
-})
+  },
+});
