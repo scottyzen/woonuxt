a19e1c68b7f9eaeec4e9e795041be88e00b75ec8
beljp <jplinstra@gmail.com>
2025-10-24 10:25:44 +0200

    Update nuxt.config.ts
    aangepast cdn uitgezet voor afbeeldingen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,44 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
+  // Get all the pages, components, composables and plugins from the parent theme
   extends: ['./woonuxt_base'],
+
   components: [{ path: './components', pathPrefix: false }],
 
+  // Gebruik @nuxt/image, maar zonder Netlify-proxy
   modules: ['@nuxt/image'],
 
-  // Geen Netlify Image CDN: directe URLs
+  // ⛔ Geen Netlify Image CDN: gebruik directe image-URL's
   image: {
     provider: 'static'
   },
 
-  // Zorg dat je WP/GraphQL host beschikbaar is voor de app
+  // Maak GQL_HOST beschikbaar voor je app (voor o.a. vertalingen/menus etc.)
+  // Zet dezelfde env var ook in Netlify → Site settings → Environment variables
   runtimeConfig: {
     public: {
-      // Zet dit óók als env var in Netlify; dit is de fallback
+      // Kies precies wat jouw theme verwacht:
+      // 1) Alleen host:
       GQL_HOST: process.env.GQL_HOST || 'https://wp.kledingzoeken.nl'
-      // Als jouw endpoint '/graphql' vereist, zet dan de volle URL hier:
+      // of, als jouw theme de volledige endpoint wil:
       // GQL_HOST: process.env.GQL_HOST || 'https://wp.kledingzoeken.nl/graphql'
     }
   },
 
+  /**
+   * Depending on your servers capabilities, you may need to adjust the following settings.
+   * It will affect the build time but also increase the reliability of the build process.
+   * If you have a server with a lot of memory and CPU, you can remove the following settings.
+   * @property {number} concurrency - How many pages to prerender at once
+   * @property {number} interval - How long to wait between prerendering pages
+   * @property {boolean} failOnError - This stops the build from failing but the page will not be statically generated
+   */
   nitro: {
-    prerender: { concurrency: 10, interval: 1000, failOnError: false },
+    prerender: {
+      concurrency: 10,
+      interval: 1000,
+      failOnError: false
+    },
     minify: true
   }
 })
-
