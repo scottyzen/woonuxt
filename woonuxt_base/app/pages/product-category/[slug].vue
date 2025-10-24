818d43f102c10645c17914454ef58c425c4d1f80
beljp <jplinstra@gmail.com>
2025-10-24 14:18:21 +0200

    Update [slug].vue

--- woonuxt_base/app/pages/product-category/[slug].vue
+++ woonuxt_base/app/pages/product-category/[slug].vue
@@ -22,34 +22,31 @@ watch(
 );
 
 useHead({
-  title: (category, i),
+  title: "categorie",
   meta: [{ hid: 'description', name: 'description', content: 'Products' }],
 });
 </script>
+
+
 <template>
+  <div>
+    <div class="container py-12">
+      <h1 class="text-3xl font-bold mb-2">{{ category.name }}</h1>
+      <div class="prose prose-sm max-w-none" v-html="category.description" />
+    </div>
 
+    <div class="container flex items-start gap-16" v-if="productsInCategory.length">
+      <Filters v-if="storeSettings.showFilters" :hide-categories="true" />
 
-  <div class="container flex items-start gap-16" v-if="productsInCategory.length">
-    <Filters v-if="storeSettings.showFilters" :hide-categories="true" />
-
-    <div class="w-full">
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo voluptate neque in odio officia assumenda nulla cumque. Veniam et tenetur accusantium, fuga pariatur
-        repellendus impedit. Quod sint odio quisquam! Sequi repellendus eum dolorum eveniet, quidem dolor necessitatibus velit sunt esse maiores. Eum odit dolorum mollitia?
-        Adipisci amet hic veniam debitis architecto nobis necessitatibus inventore iure ullam cum illo quae quam, excepturi beatae itaque exercitationem magnam quidem aspernatur
-        iusto. Quos, reprehenderit? Dignissimos autem recusandae qui sit cum velit consequatur pariatur illum omnis excepturi, adipisci ab eius, optio, temporibus iure unde
-        inventore. Maxime officiis tempore voluptatum ipsa, ab possimus atque corporis repellendus obcaecati, numquam fuga ullam doloremque ea reprehenderit earum quam culpa neque
-        dolor sint voluptates qui rem? Ipsam reiciendis non illum?
-      </p>
-
-      
-      <div class="flex items-center justify-between w-full gap-4 mt-8 md:gap-8">
-        <ProductResultCount />
-        <OrderByDropdown class="hidden md:inline-flex" v-if="storeSettings.showOrderByDropdown" />
-        <ShowFilterTrigger v-if="storeSettings.showFilters" class="md:hidden" />
+      <div class="w-full">
+        <div class="flex items-center justify-between w-full gap-4 mt-8 md:gap-8">
+          <ProductResultCount />
+          <OrderByDropdown class="hidden md:inline-flex" v-if="storeSettings.showOrderByDropdown" />
+          <ShowFilterTrigger v-if="storeSettings.showFilters" class="md:hidden" />
+        </div>
+        <ProductGrid />
       </div>
-      <ProductGrid />
     </div>
   </div>
 </template>
+
