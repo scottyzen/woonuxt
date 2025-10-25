a0d14b3888b3bc675c90a9eace5d8231556ec440
beljp <jplinstra@gmail.com>
2025-10-25 15:08:37 +0200

    Update ProductCard.vue

--- woonuxt_base/app/components/productElements/ProductCard.vue
+++ woonuxt_base/app/components/productElements/ProductCard.vue
@@ -48,7 +48,8 @@ const imagetoDisplay = computed<string>(() => {
 
 <template>
   <div class="relative group">
-    <a href="#" @click.prevent="openProduct(node.databaseId)" :title="node.name">
+    <a href="#" @click.prevent="openProduct(node.id)" :title="node.name">
+
       <SaleBadge :node class="absolute top-2 right-2" />
       <NuxtImg
         v-if="imagetoDisplay"
