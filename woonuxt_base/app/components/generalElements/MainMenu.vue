22de89955b191474756bb114337675d3340bc1b1
beljp <jplinstra@gmail.com>
2025-10-26 16:34:51 +0100

    Update MainMenu.vue

--- woonuxt_base/app/components/generalElements/MainMenu.vue
+++ woonuxt_base/app/components/generalElements/MainMenu.vue
@@ -6,10 +6,15 @@ const route = useRoute();
 <template>
   <nav>
     <NuxtLink to="/">{{ $t('general.home') }}</NuxtLink>
-    <NuxtLink to="/products">{{ $t('general.products') }}</NuxtLink>
+<!--    <NuxtLink to="/products">{{ $t('general.products') }}</NuxtLink>
     <NuxtLink to="/categories">{{ $t('shop.category', 2) }}</NuxtLink>
-    <NuxtLink to="/contact">{{ $t('general.contact') }}</NuxtLink>
-    <NuxtLink class="lg:hidden" :to="wishlistLink" :prefetch="false">Wishlist</NuxtLink>
-    <NuxtLink class="lg:hidden" to="/my-account" @click="navigateToLogin(route.fullPath)" :prefetch="false">My Account</NuxtLink>
+    <NuxtLink to="/contact">{{ $t('general.contact') }}</NuxtLink>-->
+    <NuxtLink to="/categorie/dames" class="menu-item">Dames</NuxtLink>
+    <NuxtLink to="/categorie/heren" class="menu-item">Heren</NuxtLink>
+    <NuxtLink to="/categorie/jongens" class="menu-item">Jongens</NuxtLink>
+    <NuxtLink to="/categorie/meisjes" class="menu-item">Meisjes</NuxtLink>
+    <NuxtLink to="/categorie/babys" class="menu-item">Baby's</NuxtLink>
+   <!-- <NuxtLink class="lg:hidden" :to="wishlistLink" :prefetch="false">Wishlist</NuxtLink>
+    <NuxtLink class="lg:hidden" to="/my-account" @click="navigateToLogin(route.fullPath)" :prefetch="false">My Account</NuxtLink>-->
   </nav>
 </template>
