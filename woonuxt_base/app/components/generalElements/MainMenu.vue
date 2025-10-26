b59c20d47443b9df2054f54b769bcb26f5b0e7e7
beljp <jplinstra@gmail.com>
2025-10-26 18:18:31 +0100

    Update MainMenu.vue

--- woonuxt_base/app/components/generalElements/MainMenu.vue
+++ woonuxt_base/app/components/generalElements/MainMenu.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
 const { wishlistLink, navigateToLogin } = useAuth();
 const route = useRoute();
+
+  import { useMainMenu } from '~/composables/useMainMenu'
+const { items: menuItems } = useMainMenu()
+
+
+  
 </script>
 
 <template>
+
+  <nav class="flex gap-6 text-sm font-medium">
+  <template v-for="item in menuItems" :key="item.id">
+    <div class="relative group">
+      <NuxtLink :to="item.path" class="hover:text-primary">
+        {{ item.label }}
+      </NuxtLink>
+
+      <!-- Submenu -->
+      <div
+        v-if="item.childItems?.nodes?.length"
+        class="absolute hidden group-hover:block bg-white shadow p-4 mt-2 z-50"
+      >
+        <ul class="space-y-2">
+          <li v-for="child in item.childItems.nodes" :key="child.id">
+            <NuxtLink :to="child.path" class="hover:underline">
+              {{ child.label }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </template>
+</nav>
+
+  
   <nav>
     <NuxtLink to="/">{{ $t('general.home') }}</NuxtLink>
 <!--    <NuxtLink to="/products">{{ $t('general.products') }}</NuxtLink>
