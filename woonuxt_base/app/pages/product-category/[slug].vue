7d804f954b779bd9fa18b689512d4a99d6a4ffc0
beljp <jplinstra@gmail.com>
2025-10-26 20:57:02 +0100

    Create [slug].vue

---
+++ woonuxt_base/app/pages/product-category/[slug].vue
@@ -0,0 +1,10 @@
+// woonuxt_base/app/pages/product-category/[slug].vue
+<script setup lang="ts">
+defineNuxtRouteMiddleware((to) => {
+  return navigateTo('/' + to.params.slug, { replace: true })
+})
+</script>
+
+<template>
+  <div />
+</template>
