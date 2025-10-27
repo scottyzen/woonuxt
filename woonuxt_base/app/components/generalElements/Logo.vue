9b3e98422c7fa4919e746dca282f05299a929757
beljp <jplinstra@gmail.com>
2025-10-27 16:45:35 +0100

    Update Logo.vue

--- woonuxt_base/app/components/generalElements/Logo.vue
+++ woonuxt_base/app/components/generalElements/Logo.vue
@@ -4,7 +4,7 @@ const runtimeConfig = useRuntimeConfig();
 const img = useImage();
 
 const logoUrl = runtimeConfig?.public?.LOGO ? img(runtimeConfig?.public?.LOGO) : null;
-const faviconUrl = '/logo.svg';
+const faviconUrl = '/kz-logo.svg';
 </script>
 
 <template>
