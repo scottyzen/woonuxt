96795cc4c72bb2f775f8277175db422ecda4b00c
beljp <jplinstra@gmail.com>
2025-10-27 22:07:04 +0100

    Update categories.vue

--- woonuxt_base/app/pages/categories.vue
+++ woonuxt_base/app/pages/categories.vue
@@ -11,7 +11,7 @@ useHead({
 
 <template>
   <main class="container">
-    <div v-if="productCategories?.length" class="grid grid-cols-2 gap-4 my-6 md:grid-cols-3 lg:gap-8 xl:grid-cols-4">
+    <div v-if="productCategories?.length" class="grid grid-cols-2 gap-4 my-6 md:grid-cols-5 lg:gap-8 xl:grid-cols-5">
       <CategoryCard v-for="(category, i) in productCategories" :key="i" :node="category" :image-loading="i <= 2 ? 'eager' : 'lazy'" />
     </div>
     <div v-else class="my-6 text-center text-gray-500">No categories found.</div>
