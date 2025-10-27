60bcf59887fbec5627a933b437bc4d3ecaf1e12d
beljp <jplinstra@gmail.com>
2025-10-27 13:24:14 +0100

    Update ProductCard.vue

--- woonuxt_base/app/components/productElements/ProductCard.vue
+++ woonuxt_base/app/components/productElements/ProductCard.vue
@@ -70,7 +70,7 @@ const imagetoDisplay = computed<string>(() => {
         placeholder-class="blur-xl" />
     </a>
     <div class="p-2">
-      <StarRating v-if="storeSettings.showReviews" :rating="node.averageRating" :count="node.reviewCount" />
+      <!--StarRating v-if="storeSettings.showReviews" :rating="node.averageRating" :count="node.reviewCount" /-->
       <NuxtLink v-if="node.slug" :to="`/product/${decodeURIComponent(node.slug)}`" :title="node.name">
         <h2 class="mb-2 font-light leading-tight group-hover:text-primary">{{ node.name }}</h2>
       </NuxtLink>
