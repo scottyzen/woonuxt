08e1950ae992e1d6f74e5c38e9c887d0aa281236
beljp <jplinstra@gmail.com>
2025-10-28 21:09:29 +0100

    Update useCategoryChildren.ts

--- woonuxt_base/app/composables/useCategoryChildren.ts
+++ woonuxt_base/app/composables/useCategoryChildren.ts
@@ -1,9 +1,23 @@
+import { useRoute } from 'vue-router'
 import getCategoryChildren from '~/graphql/queries/getCategoryChildren.gql'
 
-export async function useCategoryChildren(slug: string) {
-  const { data, error } = await useAsyncGql({
+export function useCategoryChildren() {
+  const route = useRoute()
+
+  const slug = computed(() => {
+    // probeer meerdere namen, afhankelijk van je routebestand
+    return route.params.slug || route.params.name || ''
+  })
+
+  const { data, error, execute } = useAsyncGql({
     query: getCategoryChildren,
-    variables: { slug }
+    variables: { slug: slug.value },
+    immediate: false, // nog niet direct uitvoeren
+  })
+
+  // Run de query zodra slug beschikbaar is
+  watchEffect(() => {
+    if (slug.value) execute({ slug: slug.value })
   })
 
   if (error.value) {
