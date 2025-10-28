a6c623557a779d6acde36ed8e3e3e558abd9dba8
beljp <jplinstra@gmail.com>
2025-10-28 11:31:12 +0100

    Update [slug].vue

--- woonuxt_base/app/pages/blog/[slug].vue
+++ woonuxt_base/app/pages/blog/[slug].vue
@@ -3,9 +3,12 @@ import GetPostBySlug from '~/graphql/queries/getPostBySlug.gql';
 import BlogContent from '~/components/blog/BlogContent.vue';
 
 const route = useRoute();
-const { data } = await useAsyncQuery(GetPostBySlug, { slug: route.params.slug });
 
-const post = computed(() => data.value?.post);
+const { data, pending, error } = await useAsyncQuery(GetPostBySlug, {
+  slug: route.params.slug,
+});
+
+const post = computed(() => data.value?.post || null);
 
 useHead(() => ({
   title: post.value?.title || 'Artikel - Kledingzoeken.nl',
@@ -16,9 +19,3 @@ useHead(() => ({
   ],
 }));
 </script>
-
-<template>
-  <section class="container mx-auto px-4 py-10">
-    <BlogContent v-if="post" :post="post" />
-  </section>
-</template>
