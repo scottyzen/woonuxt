63e2cca44be347c13c1d741cf162a8ad57765f62
beljp <jplinstra@gmail.com>
2025-10-29 15:50:34 +0100

    Update useRelatedCategories.ts

--- woonuxt_base/app/composables/useRelatedCategories.ts
+++ woonuxt_base/app/composables/useRelatedCategories.ts
@@ -1,7 +1,19 @@
 import GetRelatedCategories from '~/graphql/queries/getRelatedCategories.gql'
 
 export const useRelatedCategories = async (slug: string) => {
-  const { data } = await useAsyncQuery(GetRelatedCategories, { slug })
+  const { $graphql } = useNuxtApp() // ✅ Woonuxt GraphQL client
+
+  // SSR + caching via useAsyncData
+  const { data } = await useAsyncData(`related-categories-${slug}`, async () => {
+    const res = await $graphql.default.request(GetRelatedCategories, { slug })
+    return res
+  }, {
+    server: true,
+    lazy: false,
+    transform: (data) => data,
+    // revalidate elke 60 sec (ISR effect)
+    revalidate: 60
+  })
 
   const current = data.value?.productCategory
   const all = data.value?.productCategories?.nodes || []
