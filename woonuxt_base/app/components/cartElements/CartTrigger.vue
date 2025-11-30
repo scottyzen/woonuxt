2d0a2dd74b49285879ef0123775003969f9097c0
Scott Kennedy <scottyzen@gmail.com>
2025-11-30 00:43:41 +0000

    feat: add tabular-nums class to cart item count for better alignment

--- woonuxt_base/app/components/cartElements/CartTrigger.vue
+++ woonuxt_base/app/components/cartElements/CartTrigger.vue
@@ -9,7 +9,7 @@ const { toggleCart, cart } = useCart();
       <Transition name="popIn" mode="out-in">
         <span
           v-if="cart?.contents?.itemCount > 0"
-          class="bg-primary rounded-full text-white leading-none min-w-[16px] p-[3px] -top-1 -right-1 md:-right-2 text-[10px] absolute inline-flex justify-center items-center">
+          class="bg-primary rounded-full text-white leading-none min-w-[16px] p-[3px] -top-1 -right-1 md:-right-2 text-[10px] absolute inline-flex justify-center items-center tabular-nums">
           {{ cart?.contents?.itemCount }}
         </span>
       </Transition>
