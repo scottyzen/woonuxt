62eda76ad08a3d7d12019a26d6ced0abf245bc99
Scott Kennedy <scottyzen@gmail.com>
2026-03-25 00:55:11 +0000

    fix: update link styles for category and popular products sections

--- woonuxt_base/app/pages/index.vue
+++ woonuxt_base/app/pages/index.vue
@@ -34,7 +34,7 @@ useSeoMeta({
     <section class="container my-16">
       <div class="flex items-end justify-between">
         <h2 class="text-lg font-semibold md:text-2xl dark:text-white">{{ $t('shop.shopByCategory') }}</h2>
-        <NuxtLink class="text-primary dark:text-primary" to="/categories">{{ $t('general.viewAll') }}</NuxtLink>
+        <NuxtLink class="text-primary-dark dark:text-primary" to="/categories">{{ $t('general.viewAll') }}</NuxtLink>
       </div>
       <div class="grid justify-center grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-6">
         <CategoryCard v-for="(category, i) in productCategories" :key="i" class="w-full" :node="category" />
@@ -75,7 +75,7 @@ useSeoMeta({
     <section class="container my-16" v-if="popularProducts">
       <div class="flex items-end justify-between">
         <h2 class="text-lg font-semibold md:text-2xl dark:text-white">{{ $t('shop.popularProducts') }}</h2>
-        <NuxtLink class="text-primary dark:text-primary" to="/products">{{ $t('general.viewAll') }}</NuxtLink>
+        <NuxtLink class="font-medium text-primary-dark dark:text-primary" to="/products">{{ $t('general.viewAll') }}</NuxtLink>
       </div>
       <ProductRow :products="popularProducts" class="grid-cols-2 md:grid-cols-4 lg:grid-cols-5 mt-8" />
     </section>
