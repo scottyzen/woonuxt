5aebe733c9a187de656b613c618518c44095e1dc
Scott Kennedy <scottyzen@gmail.com>
2026-06-15 01:11:05 +0100

    refactor: streamline GraphQL calls in useCountry composable

--- woonuxt_base/app/composables/useCountry.ts
+++ woonuxt_base/app/composables/useCountry.ts
@@ -23,8 +23,8 @@ export const useCountry = () => {
     isLoadingAllowedCountries.value = true;
 
     try {
-      const gql = useWooGraphQL();
-      const response = await gql.getAllowedCountries();
+      const { getAllowedCountries } = useWooGraphQL();
+      const response = await getAllowedCountries();
       if (response.allowedCountries) {
         // Filter out null values and store the result
         allowedCountries.value = response.allowedCountries.filter((country): country is CountriesEnum => country !== null);
@@ -48,8 +48,8 @@ export const useCountry = () => {
     isLoadingCountryStates.value[countryCode] = true;
 
     try {
-      const gql = useWooGraphQL();
-      const { countryStates } = await gql.getStates({ country: countryCode });
+      const { getStates } = useWooGraphQL();
+      const { countryStates } = await getStates({ country: countryCode });
       if (countryStates) {
         countryStatesDict.value[countryCode] = countryStates as GeoLocation[];
       }
